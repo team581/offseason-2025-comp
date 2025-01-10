9ef239a587980de9c38aaf5f7c35fe0a5a18e9a9
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-09 16:30:05 -0800

    add autoChooser from snoopy repo

---
+++ src/main/java/frc/robot/autos/trailblazer/AutoChooser.java
@@ -0,0 +1,26 @@
+package frc.robot.autos.trailblazer;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.autos.trailblazer.Trailblazer;
+import frc.robot.robot_manager.RobotManager;
+import java.util.EnumSet;
+
+public class AutoChooser {
+  private final SendableChooser<BaseAuto> chooser = new SendableChooser<>();
+
+  public AutoChooser(RobotManager robotManager, Trailblazer trailblazer) {
+    SmartDashboard.putData("Autos/SelectedAuto", chooser);
+
+    for (AutoSelection selection : EnumSet.allOf(AutoSelection.class)) {
+      chooser.addOption(selection.toString(), selection.auto.apply(robotManager, trailblazer));
+    }
+
+    chooser.setDefaultOption(
+        AutoSelection.OP.toString(), AutoSelection.OP.auto.apply(robotManager, trailblazer));
+  }
+
+  public BaseAuto getSelectedAuto() {
+    return chooser.getSelected();
+  }
+}
