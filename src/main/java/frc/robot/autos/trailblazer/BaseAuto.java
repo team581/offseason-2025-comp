c8319bed8195d2b074b7fb0239f31ac4541b1ab0
Simon <87281874+simonp17@users.noreply.github.com>
2025-01-11 11:17:46 -0800

    Remove robot and auto commmands from base auto for now

--- src/main/java/frc/robot/autos/trailblazer/BaseAuto.java
+++ src/main/java/frc/robot/autos/trailblazer/BaseAuto.java
@@ -3,20 +3,15 @@ package frc.robot.autos.trailblazer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.fms.FmsSubsystem;
-import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
 
 public abstract class BaseAuto {
   protected final RobotManager robotManager;
-  protected final AutoCommands autoCommands;
-  protected final RobotCommands actions;
   protected final Trailblazer trailblazer;
 
   protected BaseAuto(RobotManager robotManager, Trailblazer trailblazer) {
     this.robotManager = robotManager;
     this.trailblazer = trailblazer;
-    this.actions = new RobotCommands(robotManager, trailblazer);
-    this.autoCommands = new AutoCommands(actions, robotManager);
   }
 
   protected abstract Command getRedAutoCommand();
