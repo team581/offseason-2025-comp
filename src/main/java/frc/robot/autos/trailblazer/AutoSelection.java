76feddbb882bc543c33893d3a1426943af44b2f6
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-11 15:09:28 -0800

    create folder just for trailblazer autos

--- src/main/java/frc/robot/autos/trailblazer/AutoSelection.java
+++ src/main/java/frc/robot/autos/trailblazer/AutoSelection.java
@@ -1,11 +1,14 @@
 package frc.robot.autos.trailblazer;
 
+import frc.robot.autos.trailblazer.autos.Three3Piece2IJKAuto;
+import frc.robot.autos.trailblazer.autos.DoNothingAuto;
+import frc.robot.autos.trailblazer.autos.PushPartnerAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
   DO_NOTHING(DoNothingAuto::new),
-  THREE_PIECE_2IJK(Auto3Piece2IJK::new),
+  THREE_PIECE_2IJK(Three3Piece2IJKAuto::new),
   PUSH_PARTNER(PushPartnerAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> auto;
