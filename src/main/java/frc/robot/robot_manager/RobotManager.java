7fa9099884ed12cdd49125ad3b1fb0702b885483
Simon <87281874+simonp17@users.noreply.github.com>
2025-01-11 12:36:48 -0800

    Replace old limelight names in robot manager

--- src/main/java/frc/robot/robot_manager/RobotManager.java
+++ src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -344,17 +344,17 @@ public class RobotManager extends StateMachine<RobotState> {
         wrist.setState(WristState.IDLE);
         intake.setState(IntakeState.IDLE_NO_GP);
         elevator.setState(ElevatorState.STOWED);
-        bottomLimelight.setState(LimelightState.TAGS);
-        topLimelight.setState(LimelightState.TAGS);
-        backLimelight.setState(LimelightState.TAGS);
+        bottomCoralLimelight.setState(LimelightState.TAGS);
+        topPurpleLimelight.setState(LimelightState.TAGS);
+        backwardsTagLimelight.setState(LimelightState.TAGS);
       }
       case CLIMBING_2_HANGING -> {
         wrist.setState(WristState.IDLE);
         intake.setState(IntakeState.IDLE_NO_GP);
         elevator.setState(ElevatorState.STOWED);
-        bottomLimelight.setState(LimelightState.TAGS);
-        topLimelight.setState(LimelightState.TAGS);
-        backLimelight.setState(LimelightState.TAGS);
+        bottomCoralLimelight.setState(LimelightState.TAGS);
+        topPurpleLimelight.setState(LimelightState.TAGS);
+        backwardsTagLimelight.setState(LimelightState.TAGS);
       }
     }
   }
