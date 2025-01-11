3b15755a847de2ff3afccc1c3a0d808e5042b7d3
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-11 15:02:37 -0800

    add new autos to auto selection

--- src/main/java/frc/robot/autos/trailblazer/AutoSelection.java
+++ src/main/java/frc/robot/autos/trailblazer/AutoSelection.java
@@ -4,7 +4,9 @@ import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
-  DO_NOTHING(DoNothingAuto::new);
+  DO_NOTHING(DoNothingAuto::new),
+  THREE_PIECE_2IJK(Auto3Piece2IJK::new),
+  PUSH_PARTNER(PushPartnerAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> auto;
 
