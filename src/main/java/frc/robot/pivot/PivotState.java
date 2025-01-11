40671d56581034472218c528880880f01cb40eed
Jonah Snider <jonah@jonahsnider.com>
2025-01-11 13:24:48 -0800

    Added pivot subsystem (#41)
    * Added pivot subsystem and a stowed position
    
    * Finished pivot subsystem
    
    * Format
    
    * Made pivot homing current based
    
    ---------
    
    Co-authored-by: rhetorr <140564619+rhetorr@users.noreply.github.com>
    Co-authored-by: Jordan <117786700+jordanjcoderman@users.noreply.github.com>

---
+++ src/main/java/frc/robot/pivot/PivotState.java
@@ -0,0 +1,15 @@
+package frc.robot.pivot;
+
+public enum PivotState {
+  HOMING(0.0),
+
+  STOWED(0.0),
+
+  CORAL_SCORE(0.0);
+
+  public final double angle;
+
+  PivotState(double angle) {
+    this.angle = angle;
+  }
+}
