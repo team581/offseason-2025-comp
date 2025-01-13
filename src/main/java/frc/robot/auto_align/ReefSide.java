27684554183371bb564f04b57ad1e4b9f92cf078
Owen <117956892+Owen756@users.noreply.github.com>
2025-01-12 16:16:19 -0800

    changed reefside pose rotation

--- src/main/java/frc/robot/auto_align/ReefSide.java
+++ src/main/java/frc/robot/auto_align/ReefSide.java
@@ -23,12 +23,12 @@ public enum ReefSide {
 
   public static Pose2d reefSidePoses(int i) {
     Pose2d[] poses = {
-      new Pose2d(3.658, 4.026, Rotation2d.fromRadians(3.142)),
-      new Pose2d(4.073, 4.746, Rotation2d.fromRadians(2.094)),
-      new Pose2d(4.905, 4.746, Rotation2d.fromRadians(1.047)),
-      new Pose2d(5.321, 4.026, Rotation2d.fromRadians(0)),
-      new Pose2d(4.905, 3.306, Rotation2d.fromRadians(-1.047)),
-      new Pose2d(4.074, 3.306, Rotation2d.fromRadians(-2.094))
+      new Pose2d(3.658, 4.026, Rotation2d.fromRadians(3.142).plus(Rotation2d.fromRotations(0.5))),
+      new Pose2d(4.073, 4.746, Rotation2d.fromRadians(2.094).plus(Rotation2d.fromRotations(0.5))),
+      new Pose2d(4.905, 4.746, Rotation2d.fromRadians(1.047).plus(Rotation2d.fromRotations(0.5))),
+      new Pose2d(5.321, 4.026, Rotation2d.fromRadians(0).plus(Rotation2d.fromRotations(0.5))),
+      new Pose2d(4.905, 3.306, Rotation2d.fromRadians(-1.047).plus(Rotation2d.fromRotations(0.5))),
+      new Pose2d(4.074, 3.306, Rotation2d.fromRadians(-2.094).plus(Rotation2d.fromRotations(0.5)))
     };
     return poses[i];
   }
