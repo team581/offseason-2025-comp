9905e0f59aea21a9118baa9e461260ace045c84b
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-16 17:49:45 -0800

    fix auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -2,13 +2,13 @@ package frc.robot.autos;
 
 import frc.robot.autos.auto_path_commands.DoNothingAuto;
 import frc.robot.autos.auto_path_commands.PushPartnerAuto;
-import frc.robot.autos.auto_path_commands.Three3Piece2IJKAuto;
+import frc.robot.autos.auto_path_commands.ThreePiece2IJKAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
   DO_NOTHING(DoNothingAuto::new),
-  THREE_PIECE_2IJK(Three3Piece2IJKAuto::new),
+  THREE_PIECE_2IJK(ThreePiece2IJKAuto::new),
   PUSH_PARTNER(PushPartnerAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> auto;
