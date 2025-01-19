e05baa9a2e3e99921fefad72262b5a24b715a2ce
Owen <117956892+Owen756@users.noreply.github.com>
2025-01-19 15:55:27 -0800

    magnetism util for swerve

--- src/main/java/frc/robot/util/MathHelpers.java
+++ src/main/java/frc/robot/util/MathHelpers.java
@@ -5,5 +5,13 @@ public class MathHelpers {
     return Math.round(value / precision) * precision;
   }
 
+  public static double sec(double radians) {
+    return (1 / Math.cos(radians));
+  }
+
+  public static double csc(double radians) {
+    return (1 / Math.sin(radians));
+  }
+
   private MathHelpers() {}
 }
