9d1c31a7e4c418181b98ae9c746dcfa4e2c62ac4
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-19 14:44:59 -0800

    comment out contraints for testing

--- src/main/java/frc/robot/autos/auto_path_commands/ThreePiece1DEC.java
+++ src/main/java/frc/robot/autos/auto_path_commands/ThreePiece1DEC.java
@@ -13,7 +13,7 @@ import frc.robot.robot_manager.RobotManager;
 
 public class ThreePiece1DEC extends BaseAuto {
   private static final AutoConstraintOptions CONSTRAINTS =
-      new AutoConstraintOptions(false, 5, 500, 8.0, 5000);
+      new AutoConstraintOptions(true, 4.75, 80, 8.0, 70);
 
   public ThreePiece1DEC(RobotManager robotManager, Trailblazer trailblazer) {
     super(robotManager, trailblazer);
@@ -34,38 +34,37 @@ public class ThreePiece1DEC extends BaseAuto {
                     new Pose2d(10.289, 3.047, Rotation2d.fromDegrees(0.0)))),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
-                new AutoPoint(new Pose2d(10.289, 3.047, Rotation2d.fromDegrees(0.0))),
+           //    CONSTRAINTS,
                 new AutoPoint(new Pose2d(11.075, 4.443, Rotation2d.fromDegrees(0.0))),
                 new AutoPoint(new Pose2d(12.493, 5.085, Rotation2d.fromDegrees(-58.45))))),
-        Commands.print("score coral (D)"),
+       Commands.print("score coral (D)"),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
+            //    CONSTRAINTS,
                 new AutoPoint(new Pose2d(14.202, 6.482, Rotation2d.fromDegrees(-135.88))),
                 new AutoPoint(new Pose2d(15.902, 7.497, Rotation2d.fromDegrees(-127.71))))),
-        Commands.print("intake"),
+       Commands.print("intake"),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
+            //    CONSTRAINTS,
                 new AutoPoint(new Pose2d(14.66, 6.482, Rotation2d.fromDegrees(-137.36))),
                 new AutoPoint(new Pose2d(13.562, 5.180, Rotation2d.fromDegrees(-119.50))))),
-        Commands.print("score coral (E)"),
+      Commands.print("score coral (E)"),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
+           //     CONSTRAINTS,
                 new AutoPoint(new Pose2d(14.66, 6.482, Rotation2d.fromDegrees(-131.81))),
-                new AutoPoint(new Pose2d(15.902, 7.497, Rotation2d.fromDegrees(0.0))))),
-        Commands.print("intake"),
+                new AutoPoint(new Pose2d(15.902, 7.497, Rotation2d.fromDegrees(-27.71))))),
+       Commands.print("intake"),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
+            //    CONSTRAINTS,
                 new AutoPoint(new Pose2d(15.014, 6.312, Rotation2d.fromDegrees(-130.68))),
                 new AutoPoint(new Pose2d(13.918, 4.933, Rotation2d.fromDegrees(-130.06))))),
         Commands.print("score coral (C)"),
         trailblazer.followSegment(
             new AutoSegment(
-                CONSTRAINTS,
+             //   CONSTRAINTS,
                 new AutoPoint(new Pose2d(14.102, 4.789, Rotation2d.fromDegrees(-123.427))))));
   }
 }
