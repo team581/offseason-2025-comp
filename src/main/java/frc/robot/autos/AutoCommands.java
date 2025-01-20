2b76efcabc47180fe2ceab19838a0d3bc882d5c3
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-01-20 10:07:52 -0800

    add l4LineupCommand to autoCommands

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -2,19 +2,29 @@ package frc.robot.autos;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
+import java.util.List;
 
 public class AutoCommands {
   private final RobotCommands robotCommands;
   private final RobotManager robotManager;
+  private final Subsystem[] requirements;
 
   public AutoCommands(RobotCommands robotCommands, RobotManager robotManager) {
     this.robotCommands = robotCommands;
     this.robotManager = robotManager;
+    var requirementsList =
+        List.of(robotManager.elevator, robotManager.wrist, robotManager.pivot, robotManager.intake);
+    requirements = requirementsList.toArray(Subsystem[]::new);
   }
 
   public Command doNothingCommand() {
     return Commands.none();
+  }
+
+  public Command l4LineupCommand() {
+    return Commands.runOnce(robotManager::l4CoralLineupRequest, requirements);
   }
 }
