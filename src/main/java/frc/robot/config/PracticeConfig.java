04b89797cfde3e0830121970a5076d482a686422
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-01-21 16:47:10 -0800

    stator current limits and de-simplify gear ratio on wrist

--- src/main/java/frc/robot/config/PracticeConfig.java
+++ src/main/java/frc/robot/config/PracticeConfig.java
@@ -121,11 +121,13 @@ class PracticeConfig {
                   .withSlot0(new Slot0Configs().withKP(0.0).withKV(0))
                   .withFeedback(
                       new FeedbackConfigs()
-                          .withSensorToMechanismRatio(66.0 + 2.0 / 3.0)) // 64/8*50/18*36/12
+                          .withSensorToMechanismRatio((64/8)*(50/18)*(36/12)))
                   .withCurrentLimits(
                       new CurrentLimitsConfigs()
                           .withSupplyCurrentLimitEnable(true)
-                          .withSupplyCurrentLimit(25.0)),
+                          .withStatorCurrentLimitEnable(true)
+                          .withSupplyCurrentLimit(25.0)
+                          .withStatorCurrentLimit(25.0)),
               0,
               180),
           new PivotConfig(
