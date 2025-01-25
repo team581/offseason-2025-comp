799aa36683f0e391bfbac6e7db86c515c40ba0b9
Simon <87281874+simonp17@users.noreply.github.com>
2025-01-25 15:27:02 -0800

    Make progress on game piece detection

--- src/main/java/frc/robot/vision/game_piece_detection/GamePieceDetection.java
+++ src/main/java/frc/robot/vision/game_piece_detection/GamePieceDetection.java
@@ -1,5 +1,6 @@
 package frc.robot.vision.game_piece_detection;
 
+import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,14 +12,14 @@ import frc.robot.vision.VisionSubsystem;
 import frc.robot.vision.results.GamePieceResult;
 
 public class GamePieceDetection {
-  private static final Pose3d LIMELIGHT_POSE_TO_ROBOT = new Pose3d(1.0,0.0,0.5, new Rotation3d(0,-10,0));
+  private static final Pose3d LIMELIGHT_POSE_TO_ROBOT = new Pose3d(0.5,0.0,0.5, new Rotation3d(0,Units.degreesToRadians(20),0));
   private static Translation2d calculateFieldRelativeTranslationFromCamera(
       double tx, double ty, Pose2d robotPoseAtCapture, Pose3d limelightToRobotOffset) {
 
     // Convert tx and ty to angles, tx on the limelight does not follow RHR
     // convention, so we need to negate by one
-
-    double thetaX = -1 * Units.degreesToRadians(tx);
+DogLog.log("GamePieceDetection/limelightPose3d", LIMELIGHT_POSE_TO_ROBOT);
+    double thetaX = -1* Units.degreesToRadians(tx);
     double thetaY = Units.degreesToRadians(ty);
     double adjustedThetaY = limelightToRobotOffset.getRotation().getY() - thetaY;
 
@@ -29,8 +30,8 @@ public class GamePieceDetection {
       yOffset =
           // .getZ() represents height from floor
           (limelightToRobotOffset.getZ() / Math.tan(adjustedThetaY))
-              // .getY() is supposed to represent forward and backward distance from center of robot
-              + Math.abs(limelightToRobotOffset.getY());
+              // .getX() is supposed to represent forward and backward distance from center of robot
+              + Math.abs(limelightToRobotOffset.getX());
     }
 
     double xOffset = yOffset * Math.tan(thetaX);
@@ -55,6 +56,7 @@ public class GamePieceDetection {
 
   public static double calculateXAngleOffsetToGamePiece(Pose2d robotPoseAtCapture, GamePieceResult visionResult){
     var gamePiecePose = calculateFieldRelativeTranslationFromCamera(robotPoseAtCapture, visionResult);
-    return LocalizationSubsystem.distanceAngleToTarget(new Pose2d(gamePiecePose, new Rotation2d()), robotPoseAtCapture).targetAngle();
+    DogLog.log("GamePieceDetection/pose", new Pose2d(gamePiecePose, new Rotation2d()));
+    return -1* (LocalizationSubsystem.distanceAngleToTarget(new Pose2d(gamePiecePose, new Rotation2d()), robotPoseAtCapture).targetAngle()- robotPoseAtCapture.getRotation().getDegrees());
   }
 }
