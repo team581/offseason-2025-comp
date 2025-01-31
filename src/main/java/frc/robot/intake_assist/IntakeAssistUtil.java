c2362086adf2a1d4ca7f7e183f3c0eba2cb8465a
Simon <87281874+simonp17@users.noreply.github.com>
2025-01-30 16:37:59 -0800

    Modify intake assist util

--- src/main/java/frc/robot/intake_assist/IntakeAssistUtil.java
+++ src/main/java/frc/robot/intake_assist/IntakeAssistUtil.java
@@ -1,47 +1,53 @@
 package frc.robot.intake_assist;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.vision.game_piece_detection.GamePieceDetectionUtil;
 import frc.robot.vision.results.GamePieceResult;
 import java.util.Optional;
 
 public class IntakeAssistUtil {
-  private static final double CORAL_ASSIST_KP = 3.0;
-  private static final double ALGAE_ASSIST_KP = 3.0;
-  private static final double MAX_ANGLE_CHANGE = 35.0;
+  private static final double CORAL_ASSIST_KP = 0.1;
+  private static final double ALGAE_ASSIST_KP = 0.1;
 
   public static ChassisSpeeds getCoralAssistSpeeds(
-      Optional<GamePieceResult> coral, ChassisSpeeds fieldRelativeInputSpeeds) {
-    return getRobotRelativeAssistSpeeds(coral, fieldRelativeInputSpeeds, CORAL_ASSIST_KP);
+      Optional<GamePieceResult> coral,
+      double robotHeading,
+      ChassisSpeeds fieldRelativeInputSpeeds) {
+    return getRobotRelativeAssistSpeeds(
+        coral, robotHeading, fieldRelativeInputSpeeds, CORAL_ASSIST_KP);
   }
 
   public static ChassisSpeeds getAlgaeAssistSpeeds(
-      Optional<GamePieceResult> algae, ChassisSpeeds fieldRelativeInputSpeeds) {
-    return getRobotRelativeAssistSpeeds(algae, fieldRelativeInputSpeeds, ALGAE_ASSIST_KP);
+      Optional<GamePieceResult> algae,
+      double robotHeading,
+      ChassisSpeeds fieldRelativeInputSpeeds) {
+    return getRobotRelativeAssistSpeeds(
+        algae, robotHeading, fieldRelativeInputSpeeds, ALGAE_ASSIST_KP);
   }
 
   private static ChassisSpeeds getRobotRelativeAssistSpeeds(
-      Optional<GamePieceResult> visionResult, ChassisSpeeds fieldRelativeInputSpeeds, double kP) {
+      Optional<GamePieceResult> visionResult,
+      double robotHeading,
+      ChassisSpeeds fieldRelativeInputSpeeds,
+      double kP) {
     if (visionResult.isEmpty()) {
       return fieldRelativeInputSpeeds;
     }
 
-    var angleError = GamePieceDetectionUtil.getRobotRelativeAngleToGamePiece(visionResult.get());
-    var modifiedAngleError = MathUtil.clamp(-MAX_ANGLE_CHANGE, MAX_ANGLE_CHANGE, angleError * kP);
+    var gamePiecePoseRobotRelative =
+        GamePieceDetectionUtil.calculateRobotRelativeTranslationFromCamera(visionResult.get());
+    var gamePiecePoseRotatedRobot =
+        gamePiecePoseRobotRelative.rotateBy(Rotation2d.fromDegrees(robotHeading));
+    var xError = gamePiecePoseRotatedRobot.getX();
+    var yError = gamePiecePoseRotatedRobot.getY();
 
-    Translation2d inputSpeedsTranslation =
-        new Translation2d(
-            fieldRelativeInputSpeeds.vxMetersPerSecond, fieldRelativeInputSpeeds.vyMetersPerSecond);
-
-    Translation2d newSpeedsTranslation =
-        inputSpeedsTranslation.rotateBy(Rotation2d.fromDegrees(modifiedAngleError));
+    var xEffort = xError * kP;
+    var yEffort = yError * kP;
 
     return new ChassisSpeeds(
-        newSpeedsTranslation.getX(),
-        newSpeedsTranslation.getY(),
+        fieldRelativeInputSpeeds.vxMetersPerSecond + xEffort,
+        fieldRelativeInputSpeeds.vyMetersPerSecond + yEffort,
         fieldRelativeInputSpeeds.omegaRadiansPerSecond);
   }
 }
