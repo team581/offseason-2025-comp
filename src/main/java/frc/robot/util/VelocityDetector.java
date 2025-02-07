499ca2be140877891edbbb8919722e4532efbe0d
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-02-06 18:38:22 -0800

    velocitydetector class and implement in intake subsystem

---
+++ src/main/java/frc/robot/util/VelocityDetector.java
@@ -0,0 +1,33 @@
+package frc.robot.util;
+
+public class VelocityDetector {
+  private boolean hasSeenMinVelocity = false;
+  private final double minVelocity;
+  private final double maxVelocity;
+  /**
+   * @param minVelocity Minimum velocity while intaking
+   * @param maxVelocity The maximum velocity that is possible if you are holding a game piece.
+   * @return VelocityDetector class
+   */
+  public VelocityDetector(double minVelocity, double maxVelocity) {
+    this.minVelocity = minVelocity;
+    this.maxVelocity = maxVelocity;
+  }
+
+  public void reset() {
+    hasSeenMinVelocity = false;
+  }
+
+  /**
+   * @param motorVelocity Current motor velocity.
+   * @return Whether the motor is holding a game piece.
+   */
+  public boolean hasGamePiece(double motorVelocity) {
+    if (hasSeenMinVelocity) {
+      return motorVelocity < maxVelocity;
+    }
+
+    hasSeenMinVelocity = motorVelocity > minVelocity;
+    return false;
+  }
+}
