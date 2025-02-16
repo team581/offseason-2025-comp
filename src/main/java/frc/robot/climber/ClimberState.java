c5eab895da91a04571e41c8fa0f2f67ac8615772
Jonah Snider <jonah@jonahsnider.com>
2025-02-16 13:30:53 -0800

    Adjust climber LINEUP position to avoid hardstop issues

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -2,7 +2,9 @@ package frc.robot.climber;
 
 public enum ClimberState {
   STOWED(167.0),
-  LINEUP(-20),
+  // Need to be very careful that this number is reachable without hitting the bumpers (lower
+  // hardstop). Otherwise the climber will do evil things.
+  LINEUP(-15),
   HANGING(115.0);
 
   public final double angle;
