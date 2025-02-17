f8dab7523fbbb2a358a9cbbba3597ffe6dc8050c
Owen <117956892+Owen756@users.noreply.github.com>
2025-02-17 09:35:45 -0800

    fix climber voltages and add check for encoder and motor opposite directions

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -1,15 +1,19 @@
 package frc.robot.climber;
 
 public enum ClimberState {
-  STOWED(167.0),
+  STOWED(167.0, 1, -1),
   // Need to be very careful that this number is reachable without hitting the bumpers (lower
   // hardstop). Otherwise the climber will do evil things.
-  LINEUP(-15),
-  HANGING(115.0);
+  LINEUP(-15, 8, -8),
+  HANGING(115.0, 12, -12);
 
   public final double angle;
+  public final double forwardsVoltage;
+  public final double backwardsVoltage;
 
-  private ClimberState(double angle) {
+  private ClimberState(double angle, double forwardVoltage, double backwardsVoltage) {
     this.angle = angle;
+    this.forwardsVoltage = forwardVoltage;
+    this.backwardsVoltage = backwardsVoltage;
   }
 }
--- src/main/java/frc/robot/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/climber/ClimberSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import frc.robot.config.RobotConfig;
 import frc.robot.util.scheduling.SubsystemPriority;
 import frc.robot.util.state_machines.StateMachine;
@@ -16,7 +17,11 @@ public class ClimberSubsystem extends StateMachine<ClimberState> {
   private static final double TOLERANCE = 3;
   private final TalonFX motor;
   private final CANcoder encoder;
+  private double motorDirection = 0;
+  private double cancoderDirection = 0;
+  private boolean climberDirectionBad = false;
   private double currentAngle;
+  private double motorAngle;
 
   public ClimberSubsystem(TalonFX motor, CANcoder encoder) {
     super(SubsystemPriority.CLIMBER, ClimberState.STOWED);
@@ -32,19 +37,16 @@ public class ClimberSubsystem extends StateMachine<ClimberState> {
   public void robotPeriodic() {
     super.robotPeriodic();
 
-    if (!CLIMBER_ENABLED) {
-      motor.disable();
-      return;
+    if (cancoderDirection != motorDirection) {
+      climberDirectionBad = true;
     }
 
-    if (atGoal()) {
+    if (climberDirectionBad || atGoal() || !CLIMBER_ENABLED) {
       motor.disable();
+    } else if (currentAngle < clamp(getState().angle)) {
+      motor.setVoltage(getState().forwardsVoltage);
     } else {
-      if (currentAngle < clamp(getState().angle)) {
-        motor.setVoltage(12.0);
-      } else {
-        motor.setVoltage(-12.0);
-      }
+      motor.setVoltage(getState().backwardsVoltage);
     }
   }
 
@@ -55,9 +57,23 @@ public class ClimberSubsystem extends StateMachine<ClimberState> {
   @Override
   protected void collectInputs() {
     currentAngle = Units.rotationsToDegrees(encoder.getAbsolutePosition().getValueAsDouble());
-    DogLog.log(
-        "Climber/MotorAngle", Units.rotationsToDegrees(motor.getPosition().getValueAsDouble()));
-    DogLog.log("Climber/CancoderAngle", currentAngle);
+    motorAngle = Units.rotationsToDegrees(motor.getPosition().getValueAsDouble());
+
+    // TODO: This actually does need to use velocity
+    cancoderDirection = Math.signum(motor.getVelocity().getValueAsDouble());
+    motorDirection = Math.signum(encoder.getVelocity().getValueAsDouble());
+
+    DogLog.log("Climber/DirectionBad", climberDirectionBad);
+    if (climberDirectionBad) {
+      DogLog.logFault("Climber Direction Bad", AlertType.kWarning);
+    }
+
+    DogLog.log("Climber/Cancoder/Direction", cancoderDirection);
+    DogLog.log("Climber/Cancoder/Angle", currentAngle);
+
+    DogLog.log("Climber/Motor/Direction", motorDirection);
+    DogLog.log("Climber/Motor/Angle", motorAngle);
+
     DogLog.log("Climber/AppliedVoltage", motor.getMotorVoltage().getValueAsDouble());
     DogLog.log("Climber/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
     DogLog.log("Climber/SupplyCurrent", motor.getSupplyCurrent().getValueAsDouble());
