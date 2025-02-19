3b35a17adae661dbfcc89822d44140800450fb35
Simon <87281874+simonp17@users.noreply.github.com>
2025-02-18 17:22:05 -0800

    Tune reef pipe distance offsets

--- src/main/java/frc/robot/auto_align/ReefPipeLevel.java
+++ src/main/java/frc/robot/auto_align/ReefPipeLevel.java
@@ -5,10 +5,10 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public enum ReefPipeLevel {
   BASE(Pose2d.kZero),
-  L1(new Pose2d(-0.59, 0, Rotation2d.kZero)),
-  L2(new Pose2d(-0.59, 0, Rotation2d.kZero)),
-  L3(new Pose2d(-0.59, 0, Rotation2d.kZero)),
-  L4(new Pose2d(-0.63, 0, Rotation2d.kZero));
+  L1(new Pose2d(-0.58, 0, Rotation2d.kZero)),
+  L2(new Pose2d(-0.58, 0, Rotation2d.kZero)),
+  L3(new Pose2d(-0.58, 0, Rotation2d.kZero)),
+  L4(new Pose2d(-0.62, 0, Rotation2d.kZero));
 
   public final Pose2d offset;
 
