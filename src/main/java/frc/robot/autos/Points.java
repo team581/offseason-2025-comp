b42ea501bd7c606abef127ee4a957524306eeb6a
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-02-22 15:19:19 -0800

    Add x & y coords for all auto starting poses

--- src/main/java/frc/robot/autos/Points.java
+++ src/main/java/frc/robot/autos/Points.java
@@ -5,12 +5,12 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.util.MathHelpers;
 
 public enum Points {
-  START_1_AND_6(Pose2d.kZero),
+  START_1_AND_6(new Pose2d(10.229, 0.758, Rotation2d.kZero)),
   START_2_AND_5(new Pose2d(10.289, 1.903, Rotation2d.kZero)),
-  START_3_AND_4(Pose2d.kZero),
-  START_4_AND_3(Pose2d.kZero),
-  START_5_AND_2(Pose2d.kZero),
-  START_6_AND_1(Pose2d.kZero),
+  START_3_AND_4(new Pose2d(10.289, 3.054, Rotation2d.kZero)),
+  START_4_AND_3(new Pose2d(10.289, 5.069, Rotation2d.kZero)),
+  START_5_AND_2(new Pose2d(10.289, 6.127, Rotation2d.kZero)),
+  START_6_AND_1(new Pose2d(10.289, 7.292, Rotation2d.kZero)),
 
   LEFT_CORAL_STATION(new Pose2d(16.1, 0.7, Rotation2d.fromDegrees(127.71))),
   RIGHT_CORAL_STATION(Pose2d.kZero);
