0cf1d33d786cee79de0bd6b67ff06f1277295c16
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-02-22 15:06:16 -0800

    change names for auto starting points

--- src/main/java/frc/robot/autos/Points.java
+++ src/main/java/frc/robot/autos/Points.java
@@ -5,9 +5,12 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.util.MathHelpers;
 
 public enum Points {
-  AUTO_START_1(Pose2d.kZero),
-  AUTO_START_2(new Pose2d(10.289, 1.903, Rotation2d.kZero)),
-  AUTO_START_3(Pose2d.kZero),
+  START_1_AND_6(Pose2d.kZero),
+  START_2_AND_5(new Pose2d(10.289, 1.903, Rotation2d.kZero)),
+  START_3_AND_4(Pose2d.kZero),
+  START_4_AND_3(Pose2d.kZero),
+  START_5_AND_2(Pose2d.kZero),
+  START_6_AND_1(Pose2d.kZero),
 
   LEFT_CORAL_STATION(new Pose2d(16.1, 0.7, Rotation2d.fromDegrees(127.71))),
   RIGHT_CORAL_STATION(Pose2d.kZero);
