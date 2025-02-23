f1b815f24944d4c462990d29e76452847b248c67
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-02-22 16:45:09 -0800

    increase intaking constraints speed, change starting poses naming, stow ealier in path after scoring, add an until condition to intaking, and add new constraints for each point in the first intake station segment for tuning

--- src/main/java/frc/robot/autos/auto_path_commands/FourPiece2IJKLAuto.java
+++ src/main/java/frc/robot/autos/auto_path_commands/FourPiece2IJKLAuto.java
@@ -16,7 +16,7 @@ import frc.robot.robot_manager.RobotState;
 
 public class FourPiece2IJKLAuto extends BaseAuto {
   private static final AutoConstraintOptions INTAKING_CONSTRAINTS =
-      new AutoConstraintOptions(3.5, 57, 4, 30);
+      new AutoConstraintOptions(4, 57, 4, 30);
   private static final AutoConstraintOptions SCORING_CONSTRAINTS =
       new AutoConstraintOptions(2, 57, 4, 30);
 
@@ -26,7 +26,7 @@ public class FourPiece2IJKLAuto extends BaseAuto {
 
   @Override
   protected Pose2d getBlueStartingPose() {
-    return Points.AUTO_START_2.bluePose;
+    return Points.START_2_AND_5.bluePose;
   }
 
   @Override
@@ -38,7 +38,7 @@ public class FourPiece2IJKLAuto extends BaseAuto {
             .followSegment(
                 new AutoSegment(
                     SCORING_CONSTRAINTS,
-                    new AutoPoint(Points.AUTO_START_2.bluePose, INTAKING_CONSTRAINTS),
+                    new AutoPoint(Points.START_2_AND_5.bluePose, INTAKING_CONSTRAINTS),
                     new AutoPoint(
                             new Pose2d(11.785, 2.0, Rotation2d.fromDegrees(60)),
                             autoCommands
@@ -160,7 +160,7 @@ public class FourPiece2IJKLAuto extends BaseAuto {
 
   @Override
   protected Pose2d getRedStartingPose() {
-    return Points.AUTO_START_2.redPose;
+    return Points.START_2_AND_5.redPose;
   }
 
   @Override
@@ -172,7 +172,7 @@ public class FourPiece2IJKLAuto extends BaseAuto {
             .followSegment(
                 new AutoSegment(
                     SCORING_CONSTRAINTS,
-                    new AutoPoint(Points.AUTO_START_2.redPose, INTAKING_CONSTRAINTS),
+                    new AutoPoint(Points.START_2_AND_5.redPose, INTAKING_CONSTRAINTS),
                     new AutoPoint(
                         new Pose2d(11.785, 2.0, Rotation2d.fromDegrees(60)),
                         autoCommands
@@ -184,16 +184,25 @@ public class FourPiece2IJKLAuto extends BaseAuto {
         autoCommands.l4ScoreAndReleaseCommand(),
 
         // INTAKE STATION
-        trailblazer.followSegment(
-            new AutoSegment(
-                INTAKING_CONSTRAINTS,
-                new AutoPoint(new Pose2d(12.132, 2.243, Rotation2d.fromDegrees(60))),
-                new AutoPoint(
-                    new Pose2d(13.636, 1.439, Rotation2d.fromDegrees(135.88)),
-                    Commands.runOnce(robotManager::stowRequest)),
-                new AutoPoint(
-                    Points.LEFT_CORAL_STATION.redPose, autoCommands.intakeStationWarmupCommand()))),
-        autoCommands.intakeStationWithTimeoutCommand(),
+        trailblazer
+            .followSegment(
+                new AutoSegment(
+                    new AutoPoint(
+                        new Pose2d(12.132, 2.243, Rotation2d.fromDegrees(60)),
+                        Commands.waitSeconds(0.25).andThen(robotManager::stowRequest),
+                        new AutoConstraintOptions(4, 57, 4, 30)),
+                    new AutoPoint(
+                        new Pose2d(13.636, 1.439, Rotation2d.fromDegrees(135.88)),
+                        new AutoConstraintOptions(2, 57, 4, 30)),
+                    new AutoPoint(
+                        Points.LEFT_CORAL_STATION.redPose,
+                        autoCommands.intakeStationWarmupCommand(),
+                        new AutoConstraintOptions(1, 57, 4, 30))),
+                false)
+            .until(
+                () ->
+                    robotManager.getState() == RobotState.SMART_STOW_1
+                        || robotManager.getState() == RobotState.SMART_STOW_2),
 
         // SCORE L4 ON J
         trailblazer
@@ -202,26 +211,31 @@ public class FourPiece2IJKLAuto extends BaseAuto {
                     SCORING_CONSTRAINTS,
                     new AutoPoint(
                         new Pose2d(14.103, 1.0, Rotation2d.fromDegrees(90.5)),
+                        autoCommands.l4WarmupCommand(ReefPipe.PIPE_J),
                         INTAKING_CONSTRAINTS),
-                    new AutoPoint(
-                        new Pose2d(12.246, 1.244, Rotation2d.fromDegrees(60.0)),
-                        autoCommands.l4WarmupCommand(ReefPipe.PIPE_J)),
+                    new AutoPoint(new Pose2d(12.246, 1.244, Rotation2d.fromDegrees(60.0))),
                     new AutoPoint(robotManager.autoAlign::getUsedScoringPose)),
                 false)
             .until(() -> robotManager.autoAlign.isTagAlignedDebounced() && robotManager.imu.isFlatDebounced()),
         autoCommands.l4ScoreAndReleaseCommand(),
 
         // INTAKE STATION
-        trailblazer.followSegment(
-            new AutoSegment(
-                INTAKING_CONSTRAINTS,
-                new AutoPoint(new Pose2d(12.246, 2.087, Rotation2d.fromDegrees(60))),
-                new AutoPoint(
-                    new Pose2d(13.872, 1.903, Rotation2d.fromDegrees(135.88)),
-                    Commands.runOnce(robotManager::stowRequest)),
-                new AutoPoint(
-                    Points.LEFT_CORAL_STATION.redPose, autoCommands.intakeStationWarmupCommand()))),
-        autoCommands.intakeStationWithTimeoutCommand(),
+        trailblazer
+            .followSegment(
+                new AutoSegment(
+                    INTAKING_CONSTRAINTS,
+                    new AutoPoint(
+                        new Pose2d(12.246, 2.087, Rotation2d.fromDegrees(60)),
+                        Commands.waitSeconds(0.25).andThen(robotManager::stowRequest)),
+                    new AutoPoint(new Pose2d(13.872, 1.903, Rotation2d.fromDegrees(135.88))),
+                    new AutoPoint(
+                        Points.LEFT_CORAL_STATION.redPose,
+                        autoCommands.intakeStationWarmupCommand())),
+                false)
+            .until(
+                () ->
+                    robotManager.getState() == RobotState.SMART_STOW_1
+                        || robotManager.getState() == RobotState.SMART_STOW_2),
 
         // SCORE L4 ON K
         autoCommands
@@ -240,21 +254,26 @@ public class FourPiece2IJKLAuto extends BaseAuto {
         autoCommands.l4ScoreAndReleaseCommand(),
 
         // INTAKE STATION
-        trailblazer.followSegment(
-            new AutoSegment(
-                INTAKING_CONSTRAINTS,
-                new AutoPoint(new Pose2d(14.284, 2.087, Rotation2d.fromDegrees(133.277))),
-                new AutoPoint(
-                    new Pose2d(15.083, 1.439, Rotation2d.fromDegrees(133.277)),
-                    Commands.runOnce(robotManager::stowRequest)),
-                new AutoPoint(
-                    Points.LEFT_CORAL_STATION.redPose, autoCommands.intakeStationWarmupCommand()))),
-        autoCommands.intakeStationWithTimeoutCommand(),
+        trailblazer
+            .followSegment(
+                new AutoSegment(
+                    INTAKING_CONSTRAINTS,
+                    new AutoPoint(
+                        new Pose2d(14.284, 2.087, Rotation2d.fromDegrees(133.277)),
+                        Commands.waitSeconds(0.25).andThen(robotManager::stowRequest)),
+                    new AutoPoint(new Pose2d(15.083, 1.439, Rotation2d.fromDegrees(133.277))),
+                    new AutoPoint(
+                        Points.LEFT_CORAL_STATION.redPose,
+                        autoCommands.intakeStationWarmupCommand())),
+                false)
+            .until(
+                () ->
+                    robotManager.getState() == RobotState.SMART_STOW_1
+                        || robotManager.getState() == RobotState.SMART_STOW_2),
 
         // SCORE L4 ON L
-        robotManager
-            .waitForStates(RobotState.IDLE_CORAL, RobotState.IDLE_NO_GP)
-            .andThen(autoCommands.l4WarmupCommand(ReefPipe.PIPE_L))
+        autoCommands
+            .l4WarmupCommand(ReefPipe.PIPE_L)
             .alongWith(
                 trailblazer
                     .followSegment(
