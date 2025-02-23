7619fcdec159134416155ea0f14fb03ece206892
Jonah Snider <jonah@jonahsnider.com>
2025-02-23 14:23:41 -0800

    Return raw AutoSelection from AutoChooser

--- src/main/java/frc/robot/autos/AutoChooser.java
+++ src/main/java/frc/robot/autos/AutoChooser.java
@@ -3,24 +3,21 @@ package frc.robot.autos;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.robot_manager.RobotManager;
-import java.util.EnumSet;
 
 public class AutoChooser {
-  private final SendableChooser<BaseAuto> chooser = new SendableChooser<>();
+  private final SendableChooser<AutoSelection> chooser = new SendableChooser<>();
 
   public AutoChooser(RobotManager robotManager, Trailblazer trailblazer) {
     SmartDashboard.putData("Autos/SelectedAuto", chooser);
 
-    for (AutoSelection selection : EnumSet.allOf(AutoSelection.class)) {
-      chooser.addOption(selection.toString(), selection.auto.apply(robotManager, trailblazer));
+    for (AutoSelection selection : AutoSelection.values()) {
+      chooser.addOption(selection.toString(), selection);
     }
 
-    chooser.setDefaultOption(
-        AutoSelection.DO_NOTHING.toString(),
-        AutoSelection.DO_NOTHING.auto.apply(robotManager, trailblazer));
+    chooser.setDefaultOption(AutoSelection.DO_NOTHING.toString(), AutoSelection.DO_NOTHING);
   }
 
-  public BaseAuto getSelectedAuto() {
+  public AutoSelection getSelectedAuto() {
     return chooser.getSelected();
   }
 }
--- src/main/java/frc/robot/autos/Autos.java
+++ src/main/java/frc/robot/autos/Autos.java
@@ -1,6 +1,7 @@
 package frc.robot.autos;
 
 import dev.doglog.DogLog;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.fms.FmsSubsystem;
@@ -11,22 +12,32 @@ import frc.robot.util.scheduling.SubsystemPriority;
 public class Autos extends LifecycleSubsystem {
   private final AutoChooser autoChooser;
   private final RobotManager robotManager;
+  private final Trailblazer trailblazer;
   private boolean hasEnabledAuto = false;
+  private Pair<AutoSelection, BaseAuto> selected;
 
   public Autos(RobotManager robotManager, Trailblazer trailblazer) {
     super(SubsystemPriority.AUTOS);
 
     autoChooser = new AutoChooser(robotManager, trailblazer);
     this.robotManager = robotManager;
+    this.trailblazer = trailblazer;
+
+    selected =
+        Pair.of(
+            AutoSelection.DO_NOTHING,
+            AutoSelection.DO_NOTHING.auto.apply(robotManager, trailblazer));
   }
 
   public Command getAutoCommand() {
-    DogLog.log("Autos/CreatedAutoCommand", autoChooser.getSelectedAuto().name());
-    return autoChooser.getSelectedAuto().getAutoCommand();
+    DogLog.log("Autos/CreatedAutoCommand", selected.getFirst().name());
+    return selected.getSecond().getAutoCommand();
   }
 
   @Override
   public void robotPeriodic() {
+    updateSelection();
+
     if (DriverStation.isAutonomousEnabled()) {
       hasEnabledAuto = true;
     }
@@ -38,9 +49,18 @@ public class Autos extends LifecycleSubsystem {
   }
 
   private void resetPoseForAuto() {
-    var auto = autoChooser.getSelectedAuto();
+    var auto = selected.getSecond();
     var startingPose =
         FmsSubsystem.isRedAlliance() ? auto.getRedStartingPose() : auto.getBlueStartingPose();
     robotManager.localization.resetPose(startingPose);
+  }
+
+  private void updateSelection() {
+    if (selected.getFirst() != autoChooser.getSelectedAuto()) {
+      selected =
+          Pair.of(
+              autoChooser.getSelectedAuto(),
+              autoChooser.getSelectedAuto().auto.apply(robotManager, trailblazer));
+    }
   }
 }
