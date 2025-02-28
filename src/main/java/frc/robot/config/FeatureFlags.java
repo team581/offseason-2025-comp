62b4b6ad0086a601a8cb2fe9660063ff88939318
Jonah Snider <jonah@jonahsnider.com>
2025-02-27 21:33:01 -0800

    Add FIELD_CALIBRATION feature flag to help with scoring pose tuning

--- src/main/java/frc/robot/config/FeatureFlags.java
+++ src/main/java/frc/robot/config/FeatureFlags.java
@@ -19,5 +19,8 @@ public class FeatureFlags {
   public static final BooleanSupplier INTAKE_VELOCITY_CORAL_DETECTION =
       FeatureFlag.of("Intake/CoralVelocityDetection", true);
 
+  public static final BooleanSupplier FIELD_CALIBRATION =
+      FeatureFlag.of("FieldCalibration", RobotConfig.IS_DEVELOPMENT);
+
   private FeatureFlags() {}
 }
