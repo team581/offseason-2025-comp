09e8d8f8928e9cc7c361ac5a6cbedd64e484017e
Simon <87281874+simonp17@users.noreply.github.com>
2025-02-28 20:48:35 -0800

    Climber changes

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -3,9 +3,9 @@ package frc.robot.climber;
 public enum ClimberState {
   STOWED(210, 2, -2),
   LINEUP(0, 12, -12),
-  HANGING(165.0, 12, -12),
+  HANGING(156.0, 12, -12),
   HANGING_2(160.0, 12, -12),
-  HANGING_3(156.0, 12, -12);
+  HANGING_3(165.0, 12, -12);
 
   public final double angle;
   public final double forwardsVoltage;
