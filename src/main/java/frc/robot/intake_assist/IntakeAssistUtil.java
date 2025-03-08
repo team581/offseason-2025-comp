ebfe18e581d3960c6787035a5e2d987eb3df0706
Simon <87281874+simonp17@users.noreply.github.com>
2025-03-08 15:06:47 -0800

    tune intake assist p

--- src/main/java/frc/robot/intake_assist/IntakeAssistUtil.java
+++ src/main/java/frc/robot/intake_assist/IntakeAssistUtil.java
@@ -13,7 +13,7 @@ public class IntakeAssistUtil {
   private static final double INITIAL_LINEUP_DISTANCE_THRESHOLD = 0.05;
   private static final double INITIAL_LINEUP_DISTANCE_FROM_CORAL = 0.9;
   private static final double FINAL_SHOVE_DISTANCE_FROM_CORAL = 0.5;
-  private static final double CORAL_ASSIST_KP = 2.0;
+  private static final double CORAL_ASSIST_KP = 3.0;
   private static final double ALGAE_ASSIST_KP = 2.0;
 
   public static ChassisSpeeds getCoralAssistSpeeds(
