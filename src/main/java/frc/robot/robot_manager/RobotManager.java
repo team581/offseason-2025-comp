3d3f14e4e10b23d4d36bd1439a0f01dd1f23ff3a
Jonah Snider <jonah@jonahsnider.com>
2025-03-08 16:07:16 -0800

    Fix potential crash in coral map logging

--- src/main/java/frc/robot/robot_manager/RobotManager.java
+++ src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -994,18 +994,21 @@ public class RobotManager extends StateMachine<RobotState> {
         swerve.snapsDriveRequest(SnapUtil.getCoralStationAngle(localization.getPose()) - 180.0);
       }
       case INTAKE_ASSIST_CORAL_FLOOR_HORIZONTAL -> {
-        if (DriverStation.isTeleop() && maybeBestCoralMapTranslation.isPresent()) {
-          Pose2d lookaheadPose = localization.getLookaheadPose(0.5);
-          DogLog.log("IntakeAssist/LookaheadPose", lookaheadPose);
-          swerve.setFieldRelativeCoralAssistSpeedsOffset(
-              IntakeAssistUtil.getAssistSpeedsFromPose(
-                  maybeBestCoralMapTranslation.get(), lookaheadPose));
-          swerve.coralAlignmentDriveRequest();
-        } else if (DriverStation.isAutonomous()) {
-          var coralSnapAngle =
-              IntakeAssistUtil.getIntakeAssistAngle(
-                  maybeBestCoralMapTranslation.get().getTranslation(), localization.getPose());
-          swerve.snapsDriveRequest(coralSnapAngle);
+        if (maybeBestCoralMapTranslation.isPresent()) {
+          var bestCoralMapTranslation = maybeBestCoralMapTranslation.orElseThrow();
+
+          if (DriverStation.isTeleop()) {
+            Pose2d lookaheadPose = localization.getLookaheadPose(0.5);
+            DogLog.log("IntakeAssist/LookaheadPose", lookaheadPose);
+            swerve.setFieldRelativeCoralAssistSpeedsOffset(
+                IntakeAssistUtil.getAssistSpeedsFromPose(bestCoralMapTranslation, lookaheadPose));
+            swerve.coralAlignmentDriveRequest();
+          } else {
+            var coralSnapAngle =
+                IntakeAssistUtil.getIntakeAssistAngle(
+                    bestCoralMapTranslation.getTranslation(), localization.getPose());
+            swerve.snapsDriveRequest(coralSnapAngle);
+          }
         } else {
           swerve.normalDriveRequest();
         }
