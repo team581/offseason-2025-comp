c8a7045409b462bb544c2a514d8fca489b154fef
Simon <87281874+simonp17@users.noreply.github.com>
2025-03-14 13:28:53 -0700

    flip around climber angle order

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -3,9 +3,9 @@ package frc.robot.climber;
 public enum ClimberState {
   STOWED(98.0, -2, 2),
   LINEUP(0.0, -12, 12),
-  HANGING(90.0, -12, 12),
+  HANGING(94.0, -12, 12),
   HANGING_2(92.0, -12, 12),
-  HANGING_3(94.0, -12, 12);
+  HANGING_3(90.0, -12, 12);
 
   public final double angle;
   public final double forwardsVoltage;
