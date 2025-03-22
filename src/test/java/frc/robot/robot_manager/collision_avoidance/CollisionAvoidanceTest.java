5d5f7ed58ab82d5d74459c601189b3e636a18497
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2025-03-22 15:15:41 -0700

    collision avoidance/ a star no longer broken

--- src/test/java/frc/robot/robot_manager/collision_avoidance/CollisionAvoidanceTest.java
+++ src/test/java/frc/robot/robot_manager/collision_avoidance/CollisionAvoidanceTest.java
@@ -4,17 +4,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import frc.robot.robot_manager.SuperstructurePosition;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CollisionAvoidanceTest {
   @Test
-  public void astarTest() {
+  public void stowedUpToUpRightAstarTest() {
     var result =
         CollisionAvoidance.aStar(
-            new SuperstructurePosition(0, 90), new SuperstructurePosition(50, 0), null);
+            new SuperstructurePosition(0, 90),
+            new SuperstructurePosition(50, 0),
+            ObstructionKind.NONE);
     ArrayList<Waypoint> expected = new ArrayList<Waypoint>();
     expected.add(Waypoint.ALGAE_INTAKE_LEFT);
     expected.add(Waypoint.ALGAE_INTAKE_LEFT);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void lowLeftToStowedAstarTest() {
+    var result =
+        CollisionAvoidance.aStar(
+            new SuperstructurePosition(0, 180),
+            new SuperstructurePosition(50, -90),
+            ObstructionKind.NONE);
+    var expected = List.of(Waypoint.ALGAE_INTAKE_LEFT, Waypoint.ALGAE_INTAKE_LEFT);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void alreadyThereTest() {
+    var result =
+        CollisionAvoidance.aStar(
+            new SuperstructurePosition(0, 180),
+            new SuperstructurePosition(0, 180),
+            ObstructionKind.NONE);
+    assertEquals(List.of(Waypoint.LOLLIPOP_INTAKE_LEFT), result.orElseThrow());
+  }
+
+  @Test
+  public void getClosestNodeTest() {
+    var result = Waypoint.getClosest(new SuperstructurePosition(0, 180));
+    Waypoint expected = Waypoint.LOLLIPOP_INTAKE_LEFT;
 
     assertEquals(expected, result);
   }
