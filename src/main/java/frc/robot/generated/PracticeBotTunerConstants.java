f7cdea13a609abd358bb4c8f3d897061445aeac2
Simon <87281874+simonp17@users.noreply.github.com>
2025-03-23 11:32:59 -0700

    add pigeon config

--- src/main/java/frc/robot/generated/PracticeBotTunerConstants.java
+++ src/main/java/frc/robot/generated/PracticeBotTunerConstants.java
@@ -70,7 +70,7 @@ public class PracticeBotTunerConstants {
                   .withStatorCurrentLimitEnable(true));
   private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
   // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
-  private static final Pigeon2Configuration pigeonConfigs = null;
+  private static final Pigeon2Configuration pigeonConfigs = new Pigeon2Configuration().withMountPose(new MountPoseConfigs().withMountPoseYaw(-90));
 
   // CAN bus that the devices are located on;
   // All swerve devices must share the same CAN bus
