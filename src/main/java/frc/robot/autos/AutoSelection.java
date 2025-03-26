b2d9455295c8e4174a7724efe538ee1820e9b6a8
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-03-25 18:49:50 -0700

    auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -2,11 +2,15 @@ package frc.robot.autos;
 
 import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
+import frc.robot.autos.auto_path_commands.red.RedLollipopAuto;
+import frc.robot.autos.auto_path_commands.red.RedThreePiece2IKLAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
-  DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new);
+  DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
+  THREE_PIECE_2IKL(RedThreePiece2IKLAuto::new, RedThreePiece2IKLAuto::new),
+  LOLLIPOP(RedLollipopAuto::new, RedLollipopAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> redAuto;
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> blueAuto;
