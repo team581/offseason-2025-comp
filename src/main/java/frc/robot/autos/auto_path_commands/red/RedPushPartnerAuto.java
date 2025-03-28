7b9ae891ef844aaacf2981192699ff2f49ad4543
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-03-27 18:19:05 -0700

    format

--- src/main/java/frc/robot/autos/auto_path_commands/red/RedPushPartnerAuto.java
+++ src/main/java/frc/robot/autos/auto_path_commands/red/RedPushPartnerAuto.java
@@ -12,31 +12,27 @@ import frc.robot.autos.Points;
 import frc.robot.autos.Trailblazer;
 import frc.robot.robot_manager.RobotManager;
 
-public class RedPushPartnerAuto extends BaseAuto{
-    public RedPushPartnerAuto(RobotManager robotManager, Trailblazer trailblazer){
-      super(robotManager, trailblazer);
-    }
+public class RedPushPartnerAuto extends BaseAuto {
+  public RedPushPartnerAuto(RobotManager robotManager, Trailblazer trailblazer) {
+    super(robotManager, trailblazer);
+  }
 
-@Override
-protected Pose2d getStartingPose() {
-  return Points.START_R3_AND_B3.redPose;
-}
+  @Override
+  protected Pose2d getStartingPose() {
+    return Points.START_R3_AND_B3.redPose;
+  }
 
-@Override
-protected Command createAutoCommand(){
-  return Commands.sequence(
-    trailblazer.followSegment(
-      new AutoSegment(
-        new AutoPoint(Points.START_R3_AND_B3.redPose),
-        new AutoPoint(
-          new Pose2d(9.462, 3.054, Rotation2d.fromDegrees(0))))),
-    blocks.scorePreloadL4(Points.START_R3_AND_B3, ReefPipe.PIPE_I),
-    blocks.intakeCoralGroundPoints(Points.GROUND_INTAKE_LEFT_STATION),
-    blocks.scoreL4(ReefPipe.PIPE_J),
-    blocks.intakeCoralGroundPoints(Points.GROUND_INTAKE_LEFT_STATION),
-    blocks.scoreL4(ReefPipe.PIPE_K));
-}
-
-
-
+  @Override
+  protected Command createAutoCommand() {
+    return Commands.sequence(
+        trailblazer.followSegment(
+            new AutoSegment(
+                new AutoPoint(Points.START_R3_AND_B3.redPose),
+                new AutoPoint(new Pose2d(9.462, 3.054, Rotation2d.fromDegrees(0))))),
+        blocks.scorePreloadL4(Points.START_R3_AND_B3, ReefPipe.PIPE_I),
+        blocks.intakeCoralGroundPoints(Points.GROUND_INTAKE_LEFT_STATION),
+        blocks.scoreL4(ReefPipe.PIPE_J),
+        blocks.intakeCoralGroundPoints(Points.GROUND_INTAKE_LEFT_STATION),
+        blocks.scoreL4(ReefPipe.PIPE_K));
+  }
 }
