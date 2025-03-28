072a631f9d4f5976a84a8c877e4992b657b1192f
Jonah Snider <jonah@jonahsnider.com>
2025-03-28 12:22:10 -0700

    Add SuperstructureLimits for fine tuning superstructure motion in collision avoidance

--- src/main/java/frc/robot/robot_manager/collision_avoidance/WaypointEdge.java
+++ src/main/java/frc/robot/robot_manager/collision_avoidance/WaypointEdge.java
@@ -1,28 +1,52 @@
 package frc.robot.robot_manager.collision_avoidance;
 
+import java.util.Optional;
+
 public record WaypointEdge(
     /** The cost associated with the motion connecting the nodes on this edge. */
     double cost,
     /** Whether this motion is safe when the left side of the robot is obstructed. */
     boolean safeWhenLeftBlocked,
     /** Whether this motion is safe when the right side of the robot is obstructed. */
-    boolean safeWhenRightBlocked) {
-
+    boolean safeWhenRightBlocked,
+    /**
+     * The constraints for superstructure motion to use when moving along this edge. If empty,
+     * regular motion magic parameters are used.
+     */
+    Optional<SuperstructureLimits> limits) {
   public static WaypointEdge rightUnblocked(Waypoint from, Waypoint to) {
-    return new WaypointEdge(from, to, true, false);
+    return new WaypointEdge(from, to, true, false, Optional.empty());
   }
 
   public static WaypointEdge leftUnblocked(Waypoint from, Waypoint to) {
-    return new WaypointEdge(from, to, false, true);
+    return new WaypointEdge(from, to, false, true, Optional.empty());
   }
 
   public static WaypointEdge alwaysSafe(Waypoint from, Waypoint to) {
-    return new WaypointEdge(from, to, true, true);
+    return new WaypointEdge(from, to, true, true, Optional.empty());
+  }
+
+  public static WaypointEdge rightUnblocked(
+      Waypoint from, Waypoint to, SuperstructureLimits limits) {
+    return new WaypointEdge(from, to, true, false, Optional.of(limits));
+  }
+
+  public static WaypointEdge leftUnblocked(
+      Waypoint from, Waypoint to, SuperstructureLimits limits) {
+    return new WaypointEdge(from, to, false, true, Optional.of(limits));
+  }
+
+  public static WaypointEdge alwaysSafe(Waypoint from, Waypoint to, SuperstructureLimits limits) {
+    return new WaypointEdge(from, to, true, true, Optional.of(limits));
   }
 
   private WaypointEdge(
-      Waypoint from, Waypoint to, boolean safeForLeftBlocked, boolean safeForRightBlocked) {
-    this(from.costFor(to), safeForLeftBlocked, safeForRightBlocked);
+      Waypoint from,
+      Waypoint to,
+      boolean safeForLeftBlocked,
+      boolean safeForRightBlocked,
+      Optional<SuperstructureLimits> limits) {
+    this(from.costFor(to), safeForLeftBlocked, safeForRightBlocked, limits);
   }
 
   public double getCost(ObstructionKind obstruction) {
