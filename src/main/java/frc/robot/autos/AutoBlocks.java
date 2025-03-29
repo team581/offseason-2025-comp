ecfd6cc2ac5588eb8b4e0df2a8242e5bc4ef7165
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-03-29 14:27:57 -0700

    add approach command to intakeLollipop autoBlock

--- src/main/java/frc/robot/autos/AutoBlocks.java
+++ src/main/java/frc/robot/autos/AutoBlocks.java
@@ -175,7 +175,8 @@ public class AutoBlocks {
         .followSegment(
             new AutoSegment(
                 BASE_CONSTRAINTS,
-                new AutoPoint(approachPoint),
+                new AutoPoint(approachPoint,
+                Commands.runOnce(()-> autoCommands.lollipopApproachCommand())),
                 new AutoPoint(
                     () ->
                         robotManager.coralMap.getLollipopIntakePose().orElse(defaultIntakingPoint),
