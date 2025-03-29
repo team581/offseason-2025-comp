dc9d73e9301480be542c7a62d01b4f07e188207a
Simon <87281874+simonp17@users.noreply.github.com>
2025-03-28 17:02:22 -0700

    Add auto filtering for coral map for coral inside reef and past lollipops

---
+++ src/test/java/frc/robot/coral_map/CoralMapTest.java
@@ -0,0 +1,20 @@
+package frc.robot.coral_map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.auto_align.AutoAlign;
+import frc.robot.vision.game_piece_detection.CoralMap;
+
+public class CoralMapTest {
+  @Test
+  void isCoralSafeForAuto() {
+    var centerOfReef = AutoAlign.getAllianceCenterOfReef();
+    var coralTranslation = new Translation2d(centerOfReef.getX()-0.5, centerOfReef.getY());
+    var result = CoralMap.isCoralInSafeSpotForAuto(coralTranslation);
+    var expected = false;
+    assertEquals(result, expected);
+  }
+}
