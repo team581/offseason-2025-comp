eeca1857fad008cf2c4be7b744cf57949ac89baa
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-03-30 14:20:47 -0700

    ground auto changes

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,13 +4,15 @@ import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopAuto;
 import frc.robot.autos.auto_path_commands.red.RedPushPartnerAuto;
-import frc.robot.autos.auto_path_commands.red.RedThreePiece2IKLAuto;
+import frc.robot.autos.auto_path_commands.red.RedThreePiece2ILKAuto;
+import frc.robot.autos.auto_path_commands.red.RedThreePiece3IKLAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
-  THREE_PIECE_2IKL(RedThreePiece2IKLAuto::new, RedThreePiece2IKLAuto::new),
+  THREE_PIECE_2ILK(RedThreePiece2ILKAuto::new, RedThreePiece2ILKAuto::new),
+  THREE_PIECE_3IKL(RedThreePiece3IKLAuto::new, RedThreePiece3IKLAuto::new),
   LOLLIPOP(RedLollipopAuto::new, RedLollipopAuto::new),
   PUSH_PARTNER(RedPushPartnerAuto::new, RedPushPartnerAuto::new);
 
---
+++ src/main/java/frc/robot/autos/auto_path_commands/red/RedThreePiece3IKLAuto.java
@@ -0,0 +1,37 @@
+package frc.robot.autos.auto_path_commands.red;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.auto_align.ReefPipe;
+import frc.robot.autos.AutoBlocks;
+import frc.robot.autos.BaseAuto;
+import frc.robot.autos.Points;
+import frc.robot.autos.Trailblazer;
+import frc.robot.robot_manager.RobotManager;
+
+public class RedThreePiece3IKLAuto extends BaseAuto {
+  public RedThreePiece3IKLAuto(RobotManager robotManager, Trailblazer trailblazer) {
+    super(robotManager, trailblazer);
+  }
+
+  @Override
+  protected Pose2d getStartingPose() {
+    return Points.START_R2_AND_B2.redPose;
+  }
+
+  @Override
+  protected Command createAutoCommand() {
+    return Commands.sequence(
+        blocks.scorePreloadL4(Points.START_R3_AND_B3.redPose, ReefPipe.PIPE_I),
+        blocks.intakeGround(
+          new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0)).transformBy(AutoBlocks.INTAKE_CORAL_GROUND_LINEUP_OFFSET),
+          new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0))),
+        blocks.scoreL4(ReefPipe.PIPE_K),
+        blocks.intakeGround(
+          new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0)).transformBy(AutoBlocks.INTAKE_CORAL_GROUND_LINEUP_OFFSET),
+          new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0))),
+        blocks.scoreL4(ReefPipe.PIPE_L));
+  }
+}
