a67d8afd98bd6e783f71a5d7318fe2c6d1bd2c72
Jonah Snider <jonah@jonahsnider.com>
2025-03-29 22:54:11 -0700

    Default to practice bot configs in simulation

--- src/main/java/frc/robot/config/RobotConfig.java
+++ src/main/java/frc/robot/config/RobotConfig.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public record RobotConfig(
     String robotName,
@@ -96,7 +97,8 @@ public record RobotConfig(
   public static final String SERIAL_NUMBER = System.getenv("serialnum");
   private static final String PRACTICE_BOT_SERIAL_NUMBER = "0329F344";
   public static final boolean IS_PRACTICE_BOT =
-      SERIAL_NUMBER != null && SERIAL_NUMBER.equals(PRACTICE_BOT_SERIAL_NUMBER);
+      RobotBase.isSimulation()
+          || (SERIAL_NUMBER != null && SERIAL_NUMBER.equals(PRACTICE_BOT_SERIAL_NUMBER));
 
   public static RobotConfig get() {
     return IS_PRACTICE_BOT ? PracticeConfig.practiceBot : CompConfig.competitionBot;
