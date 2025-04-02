37cd4525876e2d44f061087de3dfe3575eb81e71
Simon <87281874+simonp17@users.noreply.github.com>
2025-04-01 19:47:23 -0700

    decrease holding voltage for claw

--- src/main/java/frc/robot/claw/ClawSubsystem.java
+++ src/main/java/frc/robot/claw/ClawSubsystem.java
@@ -50,7 +50,7 @@ public class ClawSubsystem extends StateMachine<ClawState> {
         motor.setVoltage(12.0);
       }
       case IDLE_W_CORAL -> {
-        motor.setVoltage(1.0);
+        motor.setVoltage(0.6);
       }
       case INTAKING_ALGAE -> {
         motor.setVoltage(12);
