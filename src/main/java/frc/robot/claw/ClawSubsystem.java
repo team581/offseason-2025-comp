bfb116ea7340ccc618aade3e0374ba930c382503
Simon <87281874+simonp17@users.noreply.github.com>
2025-04-01 18:52:36 -0700

    decrease holding voltage on claw for coral to stop slipping

--- src/main/java/frc/robot/claw/ClawSubsystem.java
+++ src/main/java/frc/robot/claw/ClawSubsystem.java
@@ -50,7 +50,7 @@ public class ClawSubsystem extends StateMachine<ClawState> {
         motor.setVoltage(12.0);
       }
       case IDLE_W_CORAL -> {
-        motor.setVoltage(2.0);
+        motor.setVoltage(1.0);
       }
       case INTAKING_ALGAE -> {
         motor.setVoltage(12);
