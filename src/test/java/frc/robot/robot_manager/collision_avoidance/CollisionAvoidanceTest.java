3ab6311dc12ca5fd007ebe6aa2fa300d8f1a17cd
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2025-04-02 13:49:26 -0700

    remove bad tests

--- src/test/java/frc/robot/robot_manager/collision_avoidance/CollisionAvoidanceTest.java
+++ src/test/java/frc/robot/robot_manager/collision_avoidance/CollisionAvoidanceTest.java
@@ -6,64 +6,6 @@ import frc.robot.robot_manager.SuperstructurePosition;
 import org.junit.jupiter.api.Test;
 
 public class CollisionAvoidanceTest {
-  // @Test
-  // public void routeStowedUpToUpRightAstarTest() {
-  //   var result =
-  //       CollisionAvoidance.route(
-  //           new SuperstructurePosition(0, 90),
-  //           new SuperstructurePosition(50, 0),
-  //           ObstructionKind.NONE);
-  //   var expected = Waypoint.STOWED_UP;
-
-  //   assertEquals(expected, result);
-  // }
-  // @Test
-  // public void stowedUpToUpRightAstarTest() {
-  //   var result =
-  //       CollisionAvoidance.aStar(
-  //           new SuperstructurePosition(0, 90),
-  //           new SuperstructurePosition(50, 0),
-  //           ObstructionKind.NONE);
-  //   var expected = List.of(Waypoint.STOWED_UP, Waypoint.L4_RIGHT);
-
-  //   assertEquals(expected, new ArrayList<>(result.orElseThrow()));
-  // }
-
-  // @Test
-  // public void lowRightToStowedAstarTest() {
-  //   var result =
-  //       CollisionAvoidance.aStar(
-  //           new SuperstructurePosition(0, 0),
-  //           new SuperstructurePosition(50, -90),
-  //           ObstructionKind.NONE);
-
-  //   var expected = List.of(Waypoint.LOLLIPOP_INTAKE_RIGHT, Waypoint.L4_RIGHT, Waypoint.STOWED);
-
-  //   assertEquals(expected, new ArrayList<>(result.orElseThrow()));
-  // }
-
-  // @Test
-  // public void leftObstructedAstarTest() {
-  //   var result =
-  //       CollisionAvoidance.aStar(
-  //           new SuperstructurePosition(0, 90),
-  //           new SuperstructurePosition(50, 180),
-  //           ObstructionKind.LEFT_OBSTRUCTED);
-
-  //   assertEquals(Optional.empty(), result);
-  // }
-
-  // @Test
-  // public void rightObstructedAstarTest() {
-  //   var result =
-  //       CollisionAvoidance.aStar(
-  //           new SuperstructurePosition(0, 90),
-  //           new SuperstructurePosition(50, 0),
-  //           ObstructionKind.RIGHT_OBSTRUCTED);
-
-  //   assertEquals(Optional.empty(), result);
-  // }
-
   @Test
   public void getClosestNodeStowedTest() {
     var result = Waypoint.getClosest(new SuperstructurePosition(43, -90));
