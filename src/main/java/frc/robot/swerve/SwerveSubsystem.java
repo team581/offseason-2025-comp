b84f2b1e0caa343cd87346b6f6fe914d729d3edf
Simon <87281874+simonp17@users.noreply.github.com>
2025-04-06 14:34:39 -0700

    lower controller rotation exponent

--- src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -177,7 +177,7 @@ public class SwerveSubsystem extends StateMachine<SwerveState> {
         MathHelpers.signedExp(ControllerHelpers.deadbandJoystickValue(x, LEFT_X_DEADBAND), 2.0);
     double rightX =
         MathHelpers.signedExp(
-            ControllerHelpers.deadbandJoystickValue(theta, RIGHT_X_DEADBAND), 2.0);
+            ControllerHelpers.deadbandJoystickValue(theta, RIGHT_X_DEADBAND), 1.0);
 
     if (RobotConfig.get().swerve().invertRotation()) {
       rightX *= -1.0;
