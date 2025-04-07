352dec065b08a3588b3b8d9e83fccc883b133d06
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-04-07 16:33:24 -0700

    add lollipop variant to autoSelection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -3,6 +3,7 @@ package frc.robot.autos;
 import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopAuto;
+import frc.robot.autos.auto_path_commands.red.RedLollipopVariantAuto;
 import frc.robot.autos.auto_path_commands.red.RedPushPartnerAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece2ILKAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece3IKLAuto;
@@ -14,6 +15,7 @@ public enum AutoSelection {
   THREE_PIECE_2ILK(RedThreePiece2ILKAuto::new, RedThreePiece2ILKAuto::new),
   THREE_PIECE_3IKL(RedThreePiece3IKLAuto::new, RedThreePiece3IKLAuto::new),
   LOLLIPOP(RedLollipopAuto::new, RedLollipopAuto::new),
+  LOLLIPOP_VARIANT(RedLollipopVariantAuto::new, RedLollipopVariantAuto::new),
   PUSH_PARTNER(RedPushPartnerAuto::new, RedPushPartnerAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> redAuto;
