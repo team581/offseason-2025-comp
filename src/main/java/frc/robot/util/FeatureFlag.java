46fa21cdb0b93255ab7a77675eeeb54b49ba87ed
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-04-06 18:18:22 -0700

    Replace FeatureFlag with Doglog.tunable() (#140)

--- src/main/java/frc/robot/util/FeatureFlag.java
+++ src/main/java/frc/robot/util/FeatureFlag.java
@@ -1,17 +1,13 @@
 package frc.robot.util;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
+import dev.doglog.DogLog;
 import frc.robot.config.RobotConfig;
 import java.util.function.BooleanSupplier;
 
 public class FeatureFlag {
   public static BooleanSupplier of(String name, boolean defaultValue) {
     if (RobotConfig.IS_DEVELOPMENT) {
-      var topic = NetworkTableInstance.getDefault().getBooleanTopic("Robot/FeatureFlags/" + name);
-
-      topic.publish().set(defaultValue);
-
-      return topic.subscribe(defaultValue);
+      return DogLog.tunable("FeatureFlags/" + name, defaultValue);
     }
 
     return () -> defaultValue;
