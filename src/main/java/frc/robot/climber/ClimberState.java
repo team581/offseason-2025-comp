690a31cdfe5ee2a50b0607a9303c91dca604ac8e
Owen <117956892+Owen756@users.noreply.github.com>
2025-04-10 18:45:54 -0700

    finish todo comment

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -3,7 +3,6 @@ package frc.robot.climber;
 public enum ClimberState {
   STOPPED(98.0, 0.0),
 
-  // TODO: Bump up to 12V once verified it works
   LINEUP_FORWARD(-100, 2.0),
   LINEUP_BACKWARD(5, 2.0),
   HANGING(99.0, 12.0);
