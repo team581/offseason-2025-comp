e7017c6184838e8d4482eb82cf9abedd67ece3f9
Owen <117956892+Owen756@users.noreply.github.com>
2025-04-10 18:30:48 -0700

    adjust gp limelight position

--- src/main/java/frc/robot/config/PracticeConfig.java
+++ src/main/java/frc/robot/config/PracticeConfig.java
@@ -183,10 +183,10 @@ class PracticeConfig {
               new Pose3d(
                   Units.inchesToMeters(-8.0),
                   Units.inchesToMeters(10.78),
-                  Units.inchesToMeters(32.5),
+                  Units.inchesToMeters(32.75),
                   new Rotation3d(
                       Units.degreesToRadians(5.0),
-                      Units.degreesToRadians(30.0),
+                      Units.degreesToRadians(28.0),
                       Units.degreesToRadians(-10.0)))),
           new ArmConfig(
               RIO_CAN_NAME,
