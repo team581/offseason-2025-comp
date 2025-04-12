7480193ff96fcc8d6bd00a9cb87e8366b65124a0
Jonah Snider <jonah@jonahsnider.com>
2025-04-11 17:31:59 -0700

    Bump up climber lineup setpoint by 5 degrees

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -4,7 +4,7 @@ public enum ClimberState {
   STOPPED(98.0, 0.0),
 
   LINEUP_FORWARD(-100, 4.0),
-  LINEUP_BACKWARD(5, 2.0),
+  LINEUP_BACKWARD(10, 2.0),
   HANGING(99.0, 12.0);
 
   public final double angle;
