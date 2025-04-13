980bab2f40c99fb2c35f85a5fbca0ebad1a34c74
Jonah Snider <jonah@jonahsnider.com>
2025-04-13 15:58:21 -0700

    Delete unused lollipop autos

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,8 +4,6 @@ import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopAB1L2;
 import frc.robot.autos.auto_path_commands.red.RedLollipopAuto;
-import frc.robot.autos.auto_path_commands.red.RedLollipopDescoreKL;
-import frc.robot.autos.auto_path_commands.red.RedLollipopVariantAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece1IKLAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece2ILKAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece3GHJ;
@@ -16,10 +14,8 @@ public enum AutoSelection {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
   THREE_PIECE_2ILK(RedThreePiece2ILKAuto::new, RedThreePiece2ILKAuto::new),
   LOLLIPOP(RedLollipopAuto::new, RedLollipopAuto::new),
-  LOLLIPOP_L3_DESCRORE_KL(RedLollipopDescoreKL::new, RedLollipopDescoreKL::new),
   LOLLIPOP_AB_1L2(RedLollipopAB1L2::new, RedLollipopAB1L2::new),
 
-  LOLLIPOP_VARIANT(RedLollipopVariantAuto::new, RedLollipopVariantAuto::new),
   THREE_PIECE_3GHJ(RedThreePiece3GHJ::new, RedThreePiece3GHJ::new),
   GROUND_THREE_PIECE_1IKL(RedThreePiece1IKLAuto::new, RedThreePiece1IKLAuto::new);
 
