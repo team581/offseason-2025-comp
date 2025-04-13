7d6f03186b6dd1da99b3cba9327726e2b1e94b00
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-04-13 15:46:21 -0700

    red three piece 2IKL auto

--- src/main/java/frc/robot/autos/auto_path_commands/red/RedThreePiece2ILKAuto.java
+++ src/main/java/frc/robot/autos/auto_path_commands/red/RedThreePiece2ILKAuto.java
@@ -6,6 +6,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.auto_align.ReefPipe;
 import frc.robot.auto_align.RobotScoringSide;
+import frc.robot.autos.AutoBlocks;
+import frc.robot.autos.AutoPoint;
+import frc.robot.autos.AutoSegment;
 import frc.robot.autos.BaseAuto;
 import frc.robot.autos.Points;
 import frc.robot.autos.Trailblazer;
@@ -24,11 +27,23 @@ public class RedThreePiece2ILKAuto extends BaseAuto {
   @Override
   protected Command createAutoCommand() {
     return Commands.sequence(
-        blocks.scorePreloadL4(
-            Points.START_R2_AND_B2.redPose, ReefPipe.PIPE_I, RobotScoringSide.LEFT),
+        autoCommands.preloadCoralCommand(),
+        autoCommands.homeDeployCommand(),
+         trailblazer.followSegment(
+            new AutoSegment(
+                AutoBlocks.BASE_CONSTRAINTS_FOR_GROUND_AUTOS,
+                AutoBlocks.APPROACH_REEF_TOLERANCE,
+                new AutoPoint(new Pose2d(10.7, 1.903, Rotation2d.fromDegrees(0))),
+                new AutoPoint(
+                    new Pose2d(11.473, 2.076, Rotation2d.fromDegrees(-30)),
+                    autoCommands.l4ApproachCommand(ReefPipe.PIPE_I, RobotScoringSide.LEFT)))),
+        blocks.scoreL4(ReefPipe.PIPE_I, RobotScoringSide.LEFT),
+        autoCommands.groundIntakeToL4Command(),
         blocks.intakeGroundForL4(new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0))),
         blocks.scoreL4(ReefPipe.PIPE_L, RobotScoringSide.LEFT),
+        autoCommands.groundIntakeToL4Command(),
         blocks.intakeGroundForL4(new Pose2d(15.884, 0.994, Rotation2d.fromDegrees(0))),
-        blocks.scoreL4(ReefPipe.PIPE_K, RobotScoringSide.LEFT));
+        blocks.scoreL4(ReefPipe.PIPE_K, RobotScoringSide.LEFT),
+        autoCommands.moveToStartingPositionCommand());
   }
 }
