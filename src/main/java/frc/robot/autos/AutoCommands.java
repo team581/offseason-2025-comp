2daee89e1f0334c80b2b7529fb2a53cc049ef30e
Jonah Snider <jonah@jonahsnider.com>
2025-04-13 10:31:34 -0700

    Remove unused requirements from AutoCommands

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -3,25 +3,20 @@ package frc.robot.autos;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.auto_align.ReefPipe;
 import frc.robot.auto_align.RobotScoringSide;
 import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
 import frc.robot.robot_manager.RobotState;
 import frc.robot.robot_manager.ground_manager.GroundState;
-import java.util.List;
 
 public class AutoCommands {
   private final RobotCommands robotCommands;
   private final RobotManager robotManager;
-  private final Subsystem[] requirements;
 
   public AutoCommands(RobotCommands robotCommands, RobotManager robotManager) {
     this.robotCommands = robotCommands;
     this.robotManager = robotManager;
-    var requirementsList = List.of(robotManager.elevator, robotManager.arm, robotManager.claw);
-    requirements = requirementsList.toArray(Subsystem[]::new);
   }
 
   public Command stowRequest() {
