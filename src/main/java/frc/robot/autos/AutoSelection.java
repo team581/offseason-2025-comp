3e4814ac9d668a42280ecef418cdf317c0bac02f
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-04-15 19:31:38 -0500

    add to auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -1,6 +1,8 @@
 package frc.robot.autos;
 
 import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
+import frc.robot.autos.auto_path_commands.blue.BlueLollipopLeftABAuto;
+import frc.robot.autos.auto_path_commands.blue.BlueLollipopLeftL4Auto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftABAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftL4Auto;
@@ -13,8 +15,8 @@ import java.util.function.BiFunction;
 public enum AutoSelection {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
   THREE_PIECE_2ILK(RedThreePiece2ILKAuto::new, RedThreePiece2ILKAuto::new),
-  LOLLIPOP_LEFT_L4(RedLollipopLeftL4Auto::new, BlueDoNothingAuto::new),
-  LOLLIPOP_LEFT_AB(RedLollipopLeftABAuto::new, BlueDoNothingAuto::new),
+  LOLLIPOP_LEFT_L4(RedLollipopLeftL4Auto::new, BlueLollipopLeftL4Auto::new),
+  LOLLIPOP_LEFT_AB(RedLollipopLeftABAuto::new, BlueLollipopLeftABAuto::new),
 
   THREE_PIECE_3GHJ(RedThreePiece3GHJ::new, RedThreePiece3GHJ::new),
   GROUND_THREE_PIECE_1IKL(RedThreePiece1IKLAuto::new, RedThreePiece1IKLAuto::new);
