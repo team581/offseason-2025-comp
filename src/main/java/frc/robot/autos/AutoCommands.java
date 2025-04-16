c01e3955abf3b29fa57c6604d1fd52b77f29e088
Simon <87281874+simonp17@users.noreply.github.com>
2025-04-16 17:24:24 -0500

    add timeout to lollipop intaking

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -68,7 +68,7 @@ public class AutoCommands {
 
   public Command waitForLollipopIntakeDone() {
     return robotManager
-        .waitForStates(RobotState.CORAL_INTAKE_LOLLIPOP_PUSH, RobotState.CLAW_CORAL)
+        .waitForStates(RobotState.CORAL_INTAKE_LOLLIPOP_PUSH, RobotState.CLAW_CORAL).withTimeout(3.0)
         .withName("WaitForLollipopIntakeDoneCommand");
   }
 
