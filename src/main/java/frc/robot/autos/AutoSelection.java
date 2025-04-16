738e80d44f2b728acee8c14a84a69c26e2454e00
Simon <87281874+simonp17@users.noreply.github.com>
2025-04-15 20:40:38 -0500

    Flip AB lollipop auto vertically

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -6,6 +6,7 @@ import frc.robot.autos.auto_path_commands.blue.BlueLollipopLeftL4Auto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftABAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftL4Auto;
+import frc.robot.autos.auto_path_commands.red.RedLollipopRightABAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopRightL4Auto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece1IKLAuto;
 import frc.robot.autos.auto_path_commands.red.RedThreePiece2ILKAuto;
@@ -16,10 +17,12 @@ import java.util.function.BiFunction;
 public enum AutoSelection {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
   THREE_PIECE_2ILK(RedThreePiece2ILKAuto::new, RedThreePiece2ILKAuto::new),
+  
   LOLLIPOP_LEFT_L4(RedLollipopLeftL4Auto::new, BlueLollipopLeftL4Auto::new),
   LOLLIPOP_RIGHT_L4(RedLollipopRightL4Auto::new, BlueLollipopLeftL4Auto::new),
 
   LOLLIPOP_LEFT_AB(RedLollipopLeftABAuto::new, BlueLollipopLeftABAuto::new),
+  LOLLIPOP_RIGHT_AB(RedLollipopRightABAuto::new, BlueLollipopLeftABAuto::new),
 
   THREE_PIECE_3GHJ(RedThreePiece3GHJ::new, RedThreePiece3GHJ::new),
   GROUND_THREE_PIECE_1IKL(RedThreePiece1IKLAuto::new, RedThreePiece1IKLAuto::new);
