2cb43ab0cfc80731c974c19ba30e082732c5efc3
Jonah Snider <jonah@jonahsnider.com>
2025-04-23 12:05:04 -0700

    Add straight line auto to selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -5,17 +5,21 @@ import frc.robot.autos.auto_path_commands.blue.BlueLollipopLeftABAuto;
 import frc.robot.autos.auto_path_commands.blue.BlueLollipopLeftL4Auto;
 import frc.robot.autos.auto_path_commands.blue.BlueLollipopRightABAuto;
 import frc.robot.autos.auto_path_commands.blue.BlueLollipopRightL4Auto;
+import frc.robot.autos.auto_path_commands.blue.BlueStraightLineAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftABAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopLeftL4Auto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopRightABAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipopRightL4Auto;
+import frc.robot.autos.auto_path_commands.red.RedStraightLineAuto;
 import frc.robot.robot_manager.RobotManager;
 import frc.robot.util.trailblazer.Trailblazer;
 import java.util.function.BiFunction;
 
 public enum AutoSelection {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
+
+  STRAIGHT_LINE(RedStraightLineAuto::new, BlueStraightLineAuto::new),
 
   LOLLIPOP_LEFT_AB(RedLollipopLeftABAuto::new, BlueLollipopLeftABAuto::new),
   LOLLIPOP_RIGHT_AB(RedLollipopRightABAuto::new, BlueLollipopRightABAuto::new),
