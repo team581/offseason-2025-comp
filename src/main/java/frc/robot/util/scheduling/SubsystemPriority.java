561d7f950830919fab85818adb82bf02473301bb
Jonah Snider <jonah@jonahsnider.com>
2025-05-03 01:19:21 -0700

    Fix auto align using stale localization data

--- src/main/java/frc/robot/util/scheduling/SubsystemPriority.java
+++ src/main/java/frc/robot/util/scheduling/SubsystemPriority.java
@@ -11,15 +11,16 @@ public enum SubsystemPriority {
   GROUND_MANAGER(28),
 
   // 10-19 is for sensor subsystems
-  SWERVE(11),
+  // Auto align inputs run after localization pose is updated
+  AUTO_ALIGN(12),
   LOCALIZATION(11),
-  AUTO_ALIGN(11),
-  FMS(11),
-  IMU(11),
-  // Vision runs before localization so that it has fresh vision data for pose estimator
+  FMS(10),
+  IMU(10),
+  // Vision inputs run before localization so that it has fresh vision data for pose estimator
   VISION(10),
 
   // 0-9 is for actuator subsystems
+  SWERVE(0),
   DEPLOY(0),
   INTAKE(0),
   CLAW(0),
