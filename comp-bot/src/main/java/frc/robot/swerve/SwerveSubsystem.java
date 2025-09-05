ecf33d21fedaf7fdb182432469f1f32e434df251
Jonah Snider <jonah@jonahsnider.com>
2025-09-04 21:58:05 -0700

    Fix Rotation2d components being zero in joystick math

--- comp-bot/src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ comp-bot/src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -186,8 +186,12 @@ public class SwerveSubsystem extends StateMachine<SwerveState> implements Swerve
     var rightJoystickMagnitude =
         ControllerHelpers.getJoystickMagnitude(theta, 0, RIGHT_JOYSTICK_EXPONENT);
 
-    var translation = new Translation2d(leftJoystickMagnitude, new Rotation2d(x, y));
-    var rotation = new Translation2d(rightJoystickMagnitude, new Rotation2d(theta, 0));
+    var translation =
+        new Translation2d(
+            leftJoystickMagnitude, x == 0 && y == 0 ? Rotation2d.kZero : new Rotation2d(x, y));
+    var rotation =
+        new Translation2d(
+            rightJoystickMagnitude, theta == 0 ? Rotation2d.kZero : new Rotation2d(theta, 0));
 
     var leftX = translation.getX();
     var leftY = -1 * translation.getY();
