a78591234c21d6d354f01e96ebf62d425ef792f4
Hector <140564619+rhetorr@users.noreply.github.com>
2025-09-06 12:48:17 -0700

    unjam singulator automatically

--- offseason-bot/src/main/java/frc/robot/singulator/SingulatorSubsystem.java
+++ offseason-bot/src/main/java/frc/robot/singulator/SingulatorSubsystem.java
@@ -4,11 +4,22 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.team581.util.state_machines.StateMachine;
 import dev.doglog.DogLog;
 import frc.robot.config.RobotConfig;
+import frc.robot.singulator.SingulatorState;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class SingulatorSubsystem extends StateMachine<SingulatorState> {
   private final TalonFX leftMotor;
   private final TalonFX rightMotor;
+
+  private double rawRightCurrent = 0.0;
+  private double rawLeftCurrent = 0.0;
+
+  private final LinearFilter leftFilter = LinearFilter.movingAverage(5);
+  private final LinearFilter rightFilter = LinearFilter.movingAverage(5);
+  private double filteredRightCurrent = 0.0;
+  private double filteredLeftCurrent = 0.0;
+
+  private final double jamCurrentThreshold = 999.0;
 
   public SingulatorSubsystem(TalonFX leftMotor, TalonFX rightMotor) {
     super(SubsystemPriority.SINGULATOR, SingulatorState.IDLE);
@@ -17,6 +28,21 @@ public class SingulatorSubsystem extends StateMachine<SingulatorState> {
     rightMotor.getConfigurator().apply(RobotConfig.get().singulator().rightMotorConfig());
     this.leftMotor = leftMotor;
     this.rightMotor = rightMotor;
+  }
+
+  @Override
+  protected SingulatorState getNextState(SingulatorState currentState) {
+    return switch (currentState) {
+      default -> {
+        if (isLeftJammed()) {
+          yield SingulatorState.UNJAM_LEFT_ONLY;
+        }
+        if (isRightJammed()) {
+          yield SingulatorState.UNJAM_RIGHT_ONLY;
+        }
+        yield currentState;
+      }
+    };
   }
 
   @Override
@@ -42,11 +68,28 @@ public class SingulatorSubsystem extends StateMachine<SingulatorState> {
   }
 
   @Override
+  protected void collectInputs() {
+    rawLeftCurrent = leftMotor.getStatorCurrent().getValueAsDouble();
+    rawRightCurrent = rightMotor.getStatorCurrent().getValueAsDouble();
+
+    filteredLeftCurrent = leftFilter.calculate(rawLeftCurrent);
+    filteredRightCurrent = rightFilter.calculate(rawRightCurrent);
+  }
+
+  @Override
   public void robotPeriodic() {
     super.robotPeriodic();
 
-    DogLog.log("Singulator/Left/Current", leftMotor.getStatorCurrent().getValueAsDouble());
-    DogLog.log("Singulator/Right/Current", rightMotor.getStatorCurrent().getValueAsDouble());
+    DogLog.log("Singulator/Left/Current", filteredLeftCurrent);
+    DogLog.log("Singulator/Right/Current", filteredRightCurrent);
+  }
+
+  private boolean isLeftJammed() {
+    return filteredLeftCurrent > jamCurrentThreshold;
+  }
+
+  private boolean isRightJammed() {
+    return filteredRightCurrent > jamCurrentThreshold;
   }
 
   public void setState(SingulatorState newState) {
