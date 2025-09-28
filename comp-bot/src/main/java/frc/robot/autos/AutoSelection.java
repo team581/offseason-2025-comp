31d2c454942e19f3a161114e79104c10ac9dccf3
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-09-28 08:53:10 -0700

    Make right side lollipop auto

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,15 +4,17 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.blue.BlueLollipop4PieceR1AB;
+import frc.robot.autos.auto_path_commands.blue.BlueLollipop4PieceR6AB;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedLollipop4PieceR1AB;
+import frc.robot.autos.auto_path_commands.red.RedLollipop4PieceR6AB;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
-  LOLLIPOP_2_L2_2_L4(RedLollipop4PieceR1AB::new, BlueLollipop4PieceR1AB::new);
-  // LOLLIPOP_4_L4(RedLollipop4L4Auto::new, BlueLollipop4L4Auto::new);
+  LOLLIPOP_AB_LEFT(RedLollipop4PieceR1AB::new, BlueLollipop4PieceR1AB::new),
+  LOLLIPOP_AB_RIGHT(RedLollipop4PieceR6AB::new, BlueLollipop4PieceR6AB::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> redAuto;
   public final BiFunction<RobotManager, Trailblazer, BaseAuto> blueAuto;
