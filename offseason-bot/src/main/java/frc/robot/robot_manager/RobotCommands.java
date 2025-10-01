d10e01ae703555fda7742dbed5b60866151b0854
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-09-30 18:50:52 -0700

    robot commands

--- offseason-bot/src/main/java/frc/robot/robot_manager/RobotCommands.java
+++ offseason-bot/src/main/java/frc/robot/robot_manager/RobotCommands.java
@@ -8,27 +8,34 @@ import java.util.List;
 
 public class RobotCommands {
   private final GroundManager groundManager;
-  // private final Subsystem[] rmRequirements;
+  private final RobotManager robot;
+  private final Subsystem[] rmRequirements;
   private final Subsystem[] gmRequirements;
+  private final Subsystem[] bothRequirements;
 
-  // private final Subsystem[] bothRequirements;
-
-  public RobotCommands(GroundManager groundManager) {
+  public RobotCommands(RobotManager robot, GroundManager groundManager) {
     this.groundManager = groundManager;
+    this.robot = robot;
+
     var requirementsList =
         List.of(groundManager.deploy, groundManager.intake, groundManager.singulator);
-    // rmRequirements = requirementsList.toArray(Subsystem[]::new);
-    // requirementsList = List.of(robot.groundManager.deploy, robot.groundManager.intake);
     gmRequirements = requirementsList.toArray(Subsystem[]::new);
-    // requirementsList =
-    //     List.of(
-    //         robot.elevator,
-    //         robot.arm,
-    //         robot.claw,
-    //         robot.climber,
-    //         robot.groundManager.deploy,
-    //         robot.groundManager.intake);
-    // bothRequirements = requirementsList.toArray(Subsystem[]::new);
+    requirementsList =
+      List.of(
+      robot.elevator,
+      robot.arm,
+      robot.claw,
+      robot.climber);
+    rmRequirements = requirementsList.toArray(Subsystem[]::new);
+    requirementsList =
+      List.of(
+        robot.elevator,
+        robot.arm,
+        robot.claw,
+        robot.climber,
+        robot.groundManager.deploy,
+        robot.groundManager.intake);
+    bothRequirements = requirementsList.toArray(Subsystem[]::new);
   }
 
   public Command groundIntakeCommand() {
@@ -36,7 +43,95 @@ public class RobotCommands {
         .withName("GroundIntakeCoralCommand");
   }
 
+  public Command algaeIntakeGroundCommand() {
+    return Commands.runOnce(robot::intakeFloorAlgaeRequest, rmRequirements)
+        .withName("AlgaeIntakeGroundCommand");
+  }
+
   public Command stowCommand() {
-    return Commands.runOnce(groundManager::stowRequest, gmRequirements).withName("StowCommand");
+    return Commands.runOnce(robot::stowRequest, bothRequirements)
+        .andThen(
+            Commands.waitUntil(
+                () ->
+                    robot.elevator.atGoal()
+                        && robot.arm.atGoal()
+                        && robot.groundManager.deploy.atGoal()))
+        .withName("StowCommand");
+  }
+
+  public Command highLineupCommand() {
+    return Commands.runOnce(robot::highLineupRequest, bothRequirements)
+        .withName("HighLineupCommand");
+  }
+
+  public Command l3LineupCommand() {
+    return Commands.runOnce(robot::l3CoralApproachRequest, bothRequirements)
+        .withName("L3LineupCommand");
+  }
+
+  public Command l2LineupCommand() {
+    return Commands.runOnce(robot::l2CoralApproachRequest, bothRequirements)
+        .withName("L2LineupCommand");
+  }
+
+  public Command lowLineupCommand() {
+    return Commands.runOnce(robot::lowLineupRequest, bothRequirements).withName("LowLineupCommand");
+  }
+
+  public Command algaeReefIntakeCommand() {
+    return Commands.runOnce(robot::algaeReefIntakeRequest, rmRequirements)
+        .withName("AlgaeReefIntakeCommand");
+  }
+
+  public Command confirmScoreCommand() {
+    return Commands.runOnce(robot::confirmScoreRequest, bothRequirements)
+        .withName("ConfirmScoreCommand");
+  }
+
+  public Command forcedL1Request() {
+    return Commands.runOnce(robot::forcedL1Request, bothRequirements)
+        .withName("ForcedSensorCommand");
+  }
+
+  public Command forcedLowStowCommand() {
+    return Commands.runOnce(robot::forcedLowStowRequest, rmRequirements)
+        .withName("ForcedLowStowCommand");
+  }
+
+  public Command forcedHandoffCommand() {
+    return Commands.runOnce(robot::forcedHandoffRequest, rmRequirements)
+        .withName("ForcedHandoffCommand");
+  }
+
+  public Command scoringAlignOffCommand() {
+    return Commands.runOnce(robot::scoringAlignOffRequest, bothRequirements)
+        .withName("ScoringAlignOffCommand");
+  }
+
+  public Command climbUpCommand() {
+    return Commands.runOnce(robot::nextClimbStateRequest, rmRequirements)
+        .withName("ClimbUpCommand");
+  }
+
+  public Command climbStopCommand() {
+    return Commands.runOnce(robot::stopClimbStateRequest, rmRequirements)
+        .withName("ClimbStopCommand");
+  }
+
+  public Command unjamCommand() {
+    return Commands.runOnce(robot::unjamRequest, bothRequirements).withName("UnjamCommand");
+  }
+
+  public Command rehomeDeployCommand() {
+    return Commands.runOnce(robot.groundManager::rehomeRequest, gmRequirements)
+        .withName("RehomeDeployCommand");
+  }
+
+  public Command spinToWinCommand() {
+    return Commands.runOnce(robot::spinToWinRequest, rmRequirements).withName("SpinToWinCommand");
+  }
+
+  public Command lowStowCommand() {
+    return Commands.runOnce(robot::lowStowRequest, rmRequirements).withName("LowStowCommand");
   }
 }
