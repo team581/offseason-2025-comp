71501d47d8f6b516d47676d25d2df4a0fa4d7270
Simon <87281874+simonp17@users.noreply.github.com>
2025-10-01 15:58:05 -0700

    Turn IS_DEVELOPMENT on

--- shared/src/main/java/com/team581/GlobalConfig.java
+++ shared/src/main/java/com/team581/GlobalConfig.java
@@ -3,7 +3,7 @@ package com.team581;
 import java.util.Optional;
 
 public final class GlobalConfig {
-  public static final boolean IS_DEVELOPMENT = false;
+  public static final boolean IS_DEVELOPMENT = true;
 
   public static final String SERIAL_NUMBER =
       Optional.ofNullable(System.getenv("serialnum")).orElse("unknown");
