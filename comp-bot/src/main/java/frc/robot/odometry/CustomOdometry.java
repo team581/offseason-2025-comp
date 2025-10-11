d16d11d56ed4aa9ff96424ddb77bed42c30d0ac2
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-10-11 12:12:21 -0700

    Start math to get swerve module displacements

--- comp-bot/src/main/java/frc/robot/odometry/CustomOdometry.java
+++ comp-bot/src/main/java/frc/robot/odometry/CustomOdometry.java
@@ -15,10 +15,33 @@ public class CustomOdometry {
     this.pose = initialPoseMeters;
   }
 
+  // TODO: account for arc direction
+  private Translation2d getModuleDisplacement(
+      double previousAngle, double previousDistance, double currentAngle, double currentDistance) {
+    // First, calculate difference between previous and current angles and distances
+    double angleDifference = Math.abs(currentAngle - previousAngle);
+    double arcLength = currentDistance - previousDistance;
+
+    // Next, calculate radius
+    double radius = (Math.abs(arcLength) / angleDifference) * (180 / Math.PI);
+
+    // Then, calculate the center point of the circle that the arc is a part of, using the previous
+    // angle. The previous module translation is (0, 0) because we don't care where it starts, only
+    // the displacement
+    double circleCenterX = 0 - (radius * Math.cos(previousAngle));
+    double circleCenterY = 0 - (radius * Math.sin(previousAngle));
+
+    // Finally, calculate the current module translation
+    double displacementX = circleCenterX + (radius * Math.cos(currentAngle));
+    double displacementY = circleCenterY + (radius * Math.cos(currentAngle));
+
+    return new Translation2d(displacementX, displacementY);
+  }
+
   public Pose2d update(
       SwerveModulePosition[] previousWheelPositions, SwerveModulePosition[] currentWheelPositions) {
-    double frontLeftModuledisplacement =
-        previousWheelPositions[0].distanceMeters + currentWheelPositions[0].distanceMeters;
+    // Take averages of module displacements to get robot displacement
+    Translation2d robotDisplacement = new Translation2d();
 
     return new Pose2d(new Translation2d(), gyroAngle);
   }
