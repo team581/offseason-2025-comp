ac2d61c109375e48eee1cb27215d7b11995a05b1
Owen <117956892+Owen756@users.noreply.github.com>
2025-10-11 12:43:03 -0700

    newmembot robot manager

--- new-member-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
+++ new-member-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -5,6 +5,7 @@ import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.auto_align.AutoAlign;
+import frc.robot.auto_align.ReefPipe;
 import frc.robot.auto_align.ReefSide;
 import frc.robot.localization.LocalizationSubsystem;
 import frc.robot.swerve.SwerveSubsystem;
@@ -19,10 +20,10 @@ public class RobotManager extends StateMachine<RobotState> {
   public final SwerveSubsystem swerve;
 
   public RobotManager(
-      LocalizationSubsystem localization,
-      AutoAlign autoalign,
-      VisionSubsystem vision,
-      SwerveSubsystem swerve) {
+    LocalizationSubsystem localization,
+    AutoAlign autoalign,
+    VisionSubsystem vision,
+    SwerveSubsystem swerve) {
     super(SubsystemPriority.ROBOT_MANAGER, RobotState.CLAW_EMPTY);
 
     this.localization = localization;
@@ -37,104 +38,96 @@ public class RobotManager extends StateMachine<RobotState> {
 
   private Pose2d robotPose = new Pose2d();
   private boolean closeEnoughToReefSide = false;
+  private ReefPipe bestReefPipe = ReefPipe.PIPE_I;
   private ReefSide nearestReefSide = ReefSide.SIDE_IJ;
-  private boolean tagCameraOnline = false;
+  private boolean tagCameraOnline = vision.isAnyTagLimelightOnline();
   private boolean drivingFast = false;
   private ChassisSpeeds robotSpeeds = new ChassisSpeeds();
 
-  @Override
-  protected RobotState getNextState(RobotState currentState) {
-    return switch (currentState) {
+ @Override
+ protected RobotState getNextState(RobotState currentState) {
+     return switch (currentState) {
       case CLAW_EMPTY,
-          CLAW_ALGAE,
-          ALGAE_NET_WAITING,
-          ALGAE_PROCESSOR_WAITING,
-          CLIMBER_STOP,
-          CLIMBING_1_LINEUP,
-          CLIMBING_2_HANGING,
-          UNJAM ->
-          currentState;
+      CLAW_ALGAE,
+      ALGAE_NET_WAITING,
+      ALGAE_PROCESSOR_WAITING,
+      CLIMBER_STOP,
+      CLIMBING_1_LINEUP,
+      CLIMBING_2_HANGING,
+      UNJAM -> currentState;
 
-      case ALGAE_INTAKE_L2_APPROACH, ALGAE_INTAKE_L3_APPROACH ->
-          closeEnoughToReefSide ? currentState.getNextAlgaeIntakeState() : currentState;
+      case ALGAE_INTAKE_L2_APPROACH, ALGAE_INTAKE_L3_APPROACH -> closeEnoughToReefSide ? currentState.getNextAlgaeIntakeState() : currentState;
 
       case ALGAE_INTAKE_L2, ALGAE_INTAKE_L3 -> currentState;
 
-      case ALGAE_INTAKE_L2_HOLDING, ALGAE_INTAKE_L3_HOLDING ->
-          cameraOnlineAndFarEnoughFromReef() || drivingFast
-              ? currentState.getNextAlgaeIntakeState()
-              : currentState;
+      case ALGAE_INTAKE_L2_HOLDING, ALGAE_INTAKE_L3_HOLDING -> cameraOnlineAndFarEnoughFromReef() || drivingFast ? currentState.getNextAlgaeIntakeState() : currentState;
 
       default -> currentState;
-    };
+     };
+ }
+
+ @Override
+ protected void collectInputs() {
+  robotPose = localization.getPose();
+  bestReefPipe = autoalign.getBestReefPipe();
+  nearestReefSide = autoalign.getClosestReefSide();
+  closeEnoughToReefSide = AutoAlign.isCloseToReefSide(robotPose, nearestReefSide.getPose(), 1.0);
+  tagCameraOnline = vision.isAnyTagLimelightOnline();
+  robotSpeeds = swerve.getTeleopSpeeds();
+  drivingFast = Math.sqrt(Math.pow(robotSpeeds.vxMetersPerSecond,2) + Math.pow(robotSpeeds.vyMetersPerSecond,2)) > 5.0;
+ }
+
+ private boolean cameraOnlineAndFarEnoughFromReef() {
+  if (!tagCameraOnline) {
+    return timeout(0.5);
   }
 
-  @Override
-  protected void collectInputs() {
-    robotPose = localization.getPose();
-    nearestReefSide = autoalign.getClosestReefSide();
-    closeEnoughToReefSide = AutoAlign.isCloseToReefSide(robotPose, nearestReefSide.getPose(), 1.0);
-    tagCameraOnline = vision.isAnyTagLimelightOnline();
-    robotSpeeds = swerve.getTeleopSpeeds();
-    drivingFast =
-        Math.sqrt(
-                Math.pow(robotSpeeds.vxMetersPerSecond, 2)
-                    + Math.pow(robotSpeeds.vyMetersPerSecond, 2))
-            > 5.0;
+  var isFarEnoughFromReefSide =
+      !AutoAlign.isCloseToReefSide(robotPose, nearestReefSide.getPose(robotPose), 1.0);
+
+  return isFarEnoughFromReefSide;
+}
+
+ private void setStateFailsafe(RobotState newState) {
+  if (getState().climbingOrRehoming) {
+    return;
   }
+  setStateFromRequest(newState);
+ }
 
-  private boolean cameraOnlineAndFarEnoughFromReef() {
-    if (!tagCameraOnline) {
-      return timeout(0.5);
-    }
+ public void rehomeRequest() {
+  setStateFailsafe(RobotState.REHOME_ELEVATOR);
+ }
 
-    var isFarEnoughFromReefSide =
-        !AutoAlign.isCloseToReefSide(robotPose, nearestReefSide.getPose(robotPose), 1.0);
+ public void unjamRequest() {
+  setStateFailsafe(RobotState.UNJAM);
+ }
 
-    return isFarEnoughFromReefSide;
+ public void intakeGroundAlgaeRequest() {
+  setStateFailsafe(RobotState.ALGAE_INTAKE_FLOOR);
+ }
+
+ public void intakeL2AlgaeRequest() {
+  setStateFailsafe(RobotState.ALGAE_INTAKE_L2_APPROACH);
+ }
+
+ public void intakeL3AlgaeRequest() {
+  setStateFailsafe(RobotState.ALGAE_INTAKE_L3_APPROACH);
+ }
+
+ public void climberSequenceForward() {
+  switch (getState()) {
+    default -> setStateFromRequest(RobotState.CLIMBING_1_LINEUP);
+    case CLIMBING_1_LINEUP -> setStateFromRequest(RobotState.CLIMBING_2_HANGING);
+    case CLIMBING_2_HANGING -> setStateFromRequest(RobotState.CLIMBER_STOP);
   }
+ }
 
-  private void setStateFailsafe(RobotState newState) {
-    if (getState().climbingOrRehoming) {
-      return;
-    }
-    setStateFromRequest(newState);
+ public void climberSequenceBack() {
+  switch (getState()) {
+    case CLIMBER_STOP -> setStateFromRequest(RobotState.CLIMBING_2_HANGING);
+    case CLIMBING_2_HANGING -> setStateFromRequest(RobotState.CLIMBING_1_LINEUP);
+    case CLIMBING_1_LINEUP -> setStateFromRequest(RobotState.CLAW_EMPTY);
   }
-
-  public void rehomeRequest() {
-    setStateFailsafe(RobotState.REHOME_ELEVATOR);
-  }
-
-  public void unjamRequest() {
-    setStateFailsafe(RobotState.UNJAM);
-  }
-
-  public void intakeGroundAlgaeRequest() {
-    setStateFailsafe(RobotState.ALGAE_INTAKE_FLOOR);
-  }
-
-  public void intakeL2AlgaeRequest() {
-    setStateFailsafe(RobotState.ALGAE_INTAKE_L2_APPROACH);
-  }
-
-  public void intakeL3AlgaeRequest() {
-    setStateFailsafe(RobotState.ALGAE_INTAKE_L3_APPROACH);
-  }
-
-  public void climberSequenceForward() {
-    switch (getState()) {
-      case CLIMBING_1_LINEUP -> setStateFromRequest(RobotState.CLIMBING_2_HANGING);
-      case CLIMBING_2_HANGING -> setStateFromRequest(RobotState.CLIMBER_STOP);
-      default -> setStateFromRequest(RobotState.CLIMBING_1_LINEUP);
-    }
-  }
-
-  public void climberSequenceBack() {
-    switch (getState()) {
-      case CLIMBER_STOP -> setStateFromRequest(RobotState.CLIMBING_2_HANGING);
-      case CLIMBING_2_HANGING -> setStateFromRequest(RobotState.CLIMBING_1_LINEUP);
-      case CLIMBING_1_LINEUP -> setStateFromRequest(RobotState.CLAW_EMPTY);
-      default -> {}
-    }
-  }
+ }
 }
