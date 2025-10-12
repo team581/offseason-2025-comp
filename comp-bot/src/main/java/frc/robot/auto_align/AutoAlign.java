4e5c47591d5dcf5a4f3cad326d3d38fca42219eb
Simon <87281874+simonp17@users.noreply.github.com>
2025-10-12 15:33:35 -0700

    Make centering initial pose for after pipe selection

--- comp-bot/src/main/java/frc/robot/auto_align/AutoAlign.java
+++ comp-bot/src/main/java/frc/robot/auto_align/AutoAlign.java
@@ -135,7 +135,6 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
 
   private boolean isAligned = false;
   private boolean isAlignedDebounced = false;
-  private boolean pipeSelected = false;
   private double rawControllerXValue = 0.0;
   private double rawControllerYValue = 0.0;
   private ReefSide bestAlgaeSide = ReefSide.SIDE_AB;
@@ -143,7 +142,6 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
   private RobotScoringSide currentScoringSide = RobotScoringSide.RIGHT;
   private ReefSideOffset currentAlgaeIntakingReefSideOffset = ReefSideOffset.BASE;
   private ReefPipeLevel currentReefPipeLevel = ReefPipeLevel.L1;
-  private AutoAlignState wantedLeftRightState = AutoAlignState.SAFE_WAITING;
   private Pose2d currentPose = Pose2d.kZero;
   private Pose2d currentTargetPose = Pose2d.kZero;
   private Pose2d autoTargetPoseOverride = new Pose2d();
@@ -164,9 +162,9 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
       case SAFE_WAITING -> {
         var wantedState = getWantedPipeSideState(closestReefSide);
         yield wantedState.equals(AutoAlignState.LEFT_PIPE)
-            ? AutoAlignState.SAFE_WAITING_LEFT
+            ? AutoAlignState.SAFE_WAITING_LEFT_CENTER
             : wantedState.equals(AutoAlignState.RIGHT_PIPE)
-                ? AutoAlignState.SAFE_WAITING_RIGHT
+                ? AutoAlignState.SAFE_WAITING_RIGHT_CENTER
                 : currentState;
       }
       case SAFE_PREPARE -> {
@@ -176,6 +174,24 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
             : wantedState.equals(AutoAlignState.RIGHT_PIPE)
                 ? AutoAlignState.RIGHT_PIPE
                 : currentState;
+      }
+      case SAFE_WAITING_LEFT_CENTER -> {
+        if (currentPose.getTranslation().getDistance(currentTargetPose.getTranslation())
+            < Units.inchesToMeters(8.0)&& MathUtil.isNear(currentTargetPose.getRotation().getDegrees(), currentPose.getRotation().getDegrees(), 20.0)) {
+          yield AutoAlignState.SAFE_WAITING_LEFT_RAISING;
+        } else if (getWantedPipeSideState(closestReefSide) == AutoAlignState.RIGHT_PIPE) {
+          yield AutoAlignState.SAFE_WAITING_RIGHT_CENTER;
+        }
+        yield currentState;
+      }
+      case SAFE_WAITING_RIGHT_CENTER -> {
+        if (currentPose.getTranslation().getDistance(currentTargetPose.getTranslation())
+            < Units.inchesToMeters(8.0)&& MathUtil.isNear(currentTargetPose.getRotation().getDegrees(), currentPose.getRotation().getDegrees(), 20.0)) {
+          yield AutoAlignState.SAFE_WAITING_RIGHT_RAISING;
+        } else if (getWantedPipeSideState(closestReefSide) == AutoAlignState.LEFT_PIPE) {
+          yield AutoAlignState.SAFE_WAITING_LEFT_CENTER;
+        }
+        yield currentState;
       }
       default -> currentState;
     };
@@ -197,8 +213,12 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
 
   private Pose2d findTargetPose() {
     return switch (getState()) {
-      case SAFE_PREPARE, SAFE_WAITING, SAFE_WAITING_LEFT, SAFE_WAITING_RIGHT ->
-          getClosestReefSide().getPose(ReefSideOffset.SAFE, currentScoringSide, currentPose);
+      case SAFE_PREPARE, SAFE_WAITING ->
+      getClosestReefSide().getPose(ReefSideOffset.SAFE, currentScoringSide, currentPose);
+      case SAFE_WAITING_LEFT_CENTER -> getClosestReefSide().leftPipe.getPose(ReefPipeLevel.CENTER, currentScoringSide);
+      case SAFE_WAITING_RIGHT_CENTER -> getClosestReefSide().rightPipe.getPose(ReefPipeLevel.CENTER, currentScoringSide);
+      case SAFE_WAITING_LEFT_RAISING -> getClosestReefSide().leftPipe.getPose(ReefPipeLevel.RAISING, currentScoringSide);
+      case SAFE_WAITING_RIGHT_RAISING -> getClosestReefSide().rightPipe.getPose(ReefPipeLevel.RAISING, currentScoringSide);
       case LEFT_PIPE ->
           getClosestReefSide().leftPipe.getPose(currentReefPipeLevel, currentScoringSide);
       case RIGHT_PIPE ->
@@ -350,12 +370,12 @@ public class AutoAlign extends StateMachine<AutoAlignState> {
 
   public void setState(AutoAlignState newState) {
     switch (getState()) {
-      case SAFE_WAITING_LEFT -> {
+      case SAFE_WAITING_LEFT_CENTER, SAFE_WAITING_LEFT_RAISING -> {
         if (newState == AutoAlignState.SAFE_PREPARE) {
           setStateFromRequest(AutoAlignState.LEFT_PIPE);
         }
       }
-      case SAFE_WAITING_RIGHT -> {
+      case SAFE_WAITING_RIGHT_CENTER, SAFE_WAITING_RIGHT_RAISING -> {
         if (newState == AutoAlignState.SAFE_PREPARE) {
           setStateFromRequest(AutoAlignState.RIGHT_PIPE);
         }
--- comp-bot/src/main/java/frc/robot/auto_align/ReefPipePoses.java
+++ comp-bot/src/main/java/frc/robot/auto_align/ReefPipePoses.java
@@ -11,6 +11,7 @@ record ReefPipePoses(
     Pose2d leftL3,
     Pose2d leftL4,
     Pose2d leftRaising,
+    Pose2d leftCenter,
     Pose2d leftBackaway,
     Pose2d leftBackawayAuto,
     Pose2d rightL1,
@@ -18,6 +19,7 @@ record ReefPipePoses(
     Pose2d rightL3,
     Pose2d rightL4,
     Pose2d rightRaising,
+    Pose2d rightCenter,
     Pose2d rightBackaway,
     Pose2d rightBackawayAuto) {
   public ReefPipePoses(Pose2d base) {
@@ -28,6 +30,7 @@ record ReefPipePoses(
         base.transformBy(ReefPipeLevel.L3.leftOffset),
         base.transformBy(ReefPipeLevel.L4.leftOffset),
         base.transformBy(ReefPipeLevel.RAISING.leftOffset),
+        base.transformBy(ReefPipeLevel.CENTER.leftOffset),
         base.transformBy(ReefPipeLevel.BACK_AWAY.leftOffset),
         base.transformBy(ReefPipeLevel.BACK_AWAY_AUTO.leftOffset),
         base.transformBy(ReefPipeLevel.L1.rightOffset),
@@ -35,6 +38,7 @@ record ReefPipePoses(
         base.transformBy(ReefPipeLevel.L3.rightOffset),
         base.transformBy(ReefPipeLevel.L4.rightOffset),
         base.transformBy(ReefPipeLevel.RAISING.rightOffset),
+        base.transformBy(ReefPipeLevel.CENTER.rightOffset),
         base.transformBy(ReefPipeLevel.BACK_AWAY.rightOffset),
         base.transformBy(ReefPipeLevel.BACK_AWAY_AUTO.rightOffset));
   }
@@ -47,6 +51,7 @@ record ReefPipePoses(
       case L3 -> leftL3;
       case L4 -> leftL4;
       case RAISING -> leftRaising;
+      case CENTER -> leftCenter;
       case BACK_AWAY -> leftBackaway;
       case BACK_AWAY_AUTO -> leftBackawayAuto;
     };
@@ -60,6 +65,7 @@ record ReefPipePoses(
       case L3 -> rightL3;
       case L4 -> rightL4;
       case RAISING -> rightRaising;
+      case CENTER -> rightCenter;
       case BACK_AWAY -> rightBackaway;
       case BACK_AWAY_AUTO -> rightBackawayAuto;
     };
