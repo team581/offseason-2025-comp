4508f9669eeef45a3a5f5823e2099d9477ce495d
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-10-14 16:48:35 -0700

    elevator state

---
+++ new-member-bot/src/main/java/frc/robot/elevator/ElevatorState.java
@@ -0,0 +1,37 @@
+package frc.robot.elevator;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.networktables.DoubleSubscriber;
+
+public enum ElevatorState {
+
+  UNTUNED(0),
+  PRE_MATCH_HOMING(UNTUNED),
+  STOWED(0),
+  ALGAE_INTAKE_GROUND(0),
+  ALGAE_OUTTAKE(UNTUNED),
+  ALGAE_INTAKE_L2(0),
+  ALGAE_INTAKE_L3(0),
+  ALGAE_NET(0),
+  PROCESSOR(0),
+  CORAL_INTAKE(0),
+  CORAL_SCORE_LINEUP_L1(0.0),
+  CORAL_SCORE_RELEASE_L1(0.0),
+  CLIMBING(0.0);
+
+  private final double defaultHeight;
+  private final DoubleSubscriber tunableHeight;
+
+  private ElevatorState(double height) {
+    this.defaultHeight = height;
+    this.tunableHeight = DogLog.tunable("Elevator/State/" + name(), height);
+  }
+
+  ElevatorState(ElevatorState other) {
+    this(other.defaultHeight);
+  }
+
+  public double getHeight() {
+    return tunableHeight.get();
+  }
+}
