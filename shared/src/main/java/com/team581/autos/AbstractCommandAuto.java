4175bdd3b924c7af7e850cdf0395821d9d7155bf
Jonah Snider <jonah@jonahsnider.com>
2025-10-16 22:27:14 -0700

    Fix NullPointerException from AbstractCommandAuto
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/autos/AbstractCommandAuto.java
+++ shared/src/main/java/com/team581/autos/AbstractCommandAuto.java
@@ -1,16 +1,21 @@
 package com.team581.autos;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import java.util.Optional;
 
 public abstract class AbstractCommandAuto extends BaseAuto {
   protected final AutoTiming timing = new AutoTiming(name());
-  private final Command autoCommand = createFullAutoCommand();
+  private Optional<Command> autoCommand = Optional.empty();
 
   protected abstract Command createAutoCommand();
 
   protected abstract Command createFullAutoCommand();
 
   public Command getAutoCommand() {
-    return autoCommand;
+    if (autoCommand.isEmpty()) {
+      autoCommand = Optional.of(createFullAutoCommand());
+    }
+
+    return autoCommand.orElseThrow();
   }
 }
