5ca3680fc0919342e3e31d32ff8f6dfade21a387
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2025-10-18 17:57:00 -0700

    use reef pipe poses

--- offseason-bot/src/main/java/frc/robot/autos/Points.java
+++ offseason-bot/src/main/java/frc/robot/autos/Points.java
@@ -4,6 +4,8 @@ import com.team581.math.MathHelpers;
 import com.team581.util.FmsUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.auto_align.ReefPipe;
+import frc.robot.auto_align.ReefPipeLevel;
 
 public enum Points {
   START_R1_AND_B1(new Pose2d(10.289, 0.47, Rotation2d.fromDegrees(90))),
@@ -33,59 +35,26 @@ public enum Points {
   PRE_LOLLIPOP_2(new Pose2d(2.28, 3.964, Rotation2d.fromDegrees(0))),
   // -away from LP 2 pose, idle state
 
-  PRE_I_L4(new Pose2d(6.0, 5.5, Rotation2d.fromDegrees(240))),
-  PRE_J_L4(new Pose2d(5.5, 5.83, Rotation2d.fromDegrees(240))),
+  PRE_I_L4(ReefPipe.PIPE_I.getPose(ReefPipeLevel.RAISING)),
+  PRE_J_L4(ReefPipe.PIPE_J.getPose(ReefPipeLevel.RAISING)),
   // -robot idle state, a little before I/J pose
-  PRE_K_L4(new Pose2d(3.75, 5.75, Rotation2d.fromDegrees(300))),
-  PRE_L_L4(new Pose2d(3.441, 5.471, Rotation2d.fromDegrees(300))),
+  PRE_K_L4(ReefPipe.PIPE_K.getPose(ReefPipeLevel.RAISING)),
+  PRE_L_L4(ReefPipe.PIPE_L.getPose(ReefPipeLevel.RAISING)),
   // -robot intaking state (until singulate coral), a little before K/L pose
-  PRE_A_L4(new Pose2d(2.9, 4.19, Rotation2d.kZero)),
-  PRE_B_L4(new Pose2d(2.9, 3.86, Rotation2d.kZero)),
+  PRE_A_L4(ReefPipe.PIPE_A.getPose(ReefPipeLevel.RAISING)),
+  PRE_B_L4(ReefPipe.PIPE_B.getPose(ReefPipeLevel.RAISING)),
   // -robot intaking state (until singulate coral), a little before A/B pose
 
   I_L4_POSE(
-      new Pose2d(5.02, 4.62, Rotation2d.fromDegrees(240)),
-      new Pose2d(12.53, 3.43, Rotation2d.fromDegrees(60))),
+      ReefPipe.PIPE_I.getPose(ReefPipeLevel.L4)),
   J_L4_POSE(
-      new Pose2d(4.74, 4.78, Rotation2d.fromDegrees(240)),
-      new Pose2d(12.81, 3.27, Rotation2d.fromDegrees(60))),
+      ReefPipe.PIPE_J.getPose(ReefPipeLevel.L4)),
   K_L_POSE(
-      new Pose2d(4.24, 4.78, Rotation2d.fromDegrees(300)),
-      new Pose2d(13.31, 3.27, Rotation2d.fromDegrees(120))),
+      ReefPipe.PIPE_K.getPose(ReefPipeLevel.L4)),
   L_L4_POSE(
-      new Pose2d(3.96, 4.62, Rotation2d.fromDegrees(300)),
-      new Pose2d(13.59, 3.43, Rotation2d.fromDegrees(120))),
-  A_L4_POSE(new Pose2d(3.71, 4.19, Rotation2d.kZero), new Pose2d(13.84, 3.86, Rotation2d.k180deg)),
-  B_L4_POSE(new Pose2d(3.71, 3.86, Rotation2d.kZero), new Pose2d(13.84, 4.19, Rotation2d.k180deg));
-  // first is red second is blue
-  // PIPE_A(new Pose2d(3.71, 4.19, Rotation2d.kZero), new Pose2d(13.84, 3.86, Rotation2d.k180deg)),
-  // PIPE_B(new Pose2d(3.71, 3.86, Rotation2d.kZero), new Pose2d(13.84, 4.19, Rotation2d.k180deg)),
-  // PIPE_C(
-  //     new Pose2d(3.96, 3.43, Rotation2d.fromDegrees(60)),
-  //     new Pose2d(13.59, 4.62, Rotation2d.fromDegrees(240))),
-  // PIPE_D(
-  //     new Pose2d(4.24, 3.27, Rotation2d.fromDegrees(60)),
-  //     new Pose2d(13.31, 4.78, Rotation2d.fromDegrees(240))),
-  // PIPE_E(
-  //     new Pose2d(4.74, 3.27, Rotation2d.fromDegrees(120)),
-  //     new Pose2d(12.81, 4.78, Rotation2d.fromDegrees(300))),
-  // PIPE_F(
-  //     new Pose2d(5.02, 3.43, Rotation2d.fromDegrees(120)),
-  //     new Pose2d(12.53, 4.62, Rotation2d.fromDegrees(300))),
-  // PIPE_G(new Pose2d(5.27, 3.86, Rotation2d.k180deg), new Pose2d(12.29, 4.19, Rotation2d.kZero)),
-  // PIPE_H(new Pose2d(5.27, 4.19, Rotation2d.k180deg), new Pose2d(12.29, 3.86, Rotation2d.kZero)),
-  // PIPE_I(
-  //     new Pose2d(5.02, 4.62, Rotation2d.fromDegrees(240)),
-  //     new Pose2d(12.53, 3.43, Rotation2d.fromDegrees(60))),
-  // PIPE_J(
-  //     new Pose2d(4.74, 4.78, Rotation2d.fromDegrees(240)),
-  //     new Pose2d(12.81, 3.27, Rotation2d.fromDegrees(60))),
-  // PIPE_K(
-  //     new Pose2d(4.24, 4.78, Rotation2d.fromDegrees(300)),
-  //     new Pose2d(13.31, 3.27, Rotation2d.fromDegrees(120))),
-  // PIPE_L(
-  //     new Pose2d(3.96, 4.62, Rotation2d.fromDegrees(300)),
-  //     new Pose2d(13.59, 3.43, Rotation2d.fromDegrees(120)));
+      ReefPipe.PIPE_L.getPose(ReefPipeLevel.L4)),
+  A_L4_POSE(ReefPipe.PIPE_A.getPose(ReefPipeLevel.L4)),
+  B_L4_POSE(ReefPipe.PIPE_A.getPose(ReefPipeLevel.L4));
 
   public Pose2d redPose;
   public Pose2d bluePose;
--- offseason-bot/src/main/java/frc/robot/autos/auto_state_machines/Red5pcAuto.java
+++ offseason-bot/src/main/java/frc/robot/autos/auto_state_machines/Red5pcAuto.java
@@ -1,6 +1,9 @@
 package frc.robot.autos.auto_state_machines;
 
 import com.team581.trailblazer.Trailblazer;
+import com.team581.trailblazer.trackers.pure_pursuit.PurePursuitPathTracker;
+import com.team581.trailblazer.trackers.pure_pursuit.PurePursuitUtils;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.autos.BaseImperativeAuto;
@@ -61,7 +64,7 @@ public class Red5pcAuto extends BaseImperativeAuto<Red5pcAutoState> {
               : currentState;
       case J_L4_SCORE -> superstructureAtGoal() ? Red5pcAutoState.J_L4_POST_SCORING : currentState;
       case J_L4_POST_SCORING ->
-          superstructureAtGoal() && robotManager.swerve.driveToPoseAtGoal(currentState.pose)
+          superstructureAtGoal() && robotManager.swerve.driveToPoseAtGoal(currentState.pose)//trailblazer.followSegmentIsFinished(null)
               ? Red5pcAutoState.PRE_INTAKING
               : currentState;
 
