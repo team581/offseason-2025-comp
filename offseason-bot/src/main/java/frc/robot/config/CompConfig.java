e393877b5fac30cb4a1568952e6ecbd3a383c11f
Simon <87281874+simonp17@users.noreply.github.com>
2025-10-20 11:19:07 -0700

    Update sensor to mechanism ratios in off bot config

--- offseason-bot/src/main/java/frc/robot/config/CompConfig.java
+++ offseason-bot/src/main/java/frc/robot/config/CompConfig.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TorqueCurrentConfigs;
 import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -52,12 +53,27 @@ class CompConfig {
                           .withInverted(InvertedValue.Clockwise_Positive)
                           .withNeutralMode(NeutralModeValue.Coast))),
           new ClawConfig(
-              CANIVORE_NAME, 16, 27, false, new Debouncer(0), new TalonFXConfiguration()),
+            // TODO: BRINGUP: CHECK SENSOR FLIPPED
+              CANIVORE_NAME, 16, 27, false, new Debouncer(0),  new TalonFXConfiguration()
+                  .withCurrentLimits(
+                    // FROM COMP BOT
+                      new CurrentLimitsConfigs()
+                          .withStatorCurrentLimit(90.0)
+                          .withSupplyCurrentLimit(65.0))
+                  .withMotorOutput(
+                    new MotorOutputConfigs()
+                    // TODO: BRINGUP: MAY NEED TO INVERT
+                          .withInverted(InvertedValue.Clockwise_Positive)
+                          .withNeutralMode(NeutralModeValue.Coast))
+                  .withTorqueCurrent(
+                      new TorqueCurrentConfigs()
+                          .withPeakForwardTorqueCurrent(70.0)
+                          .withPeakReverseTorqueCurrent(70.0))),
           new DeployConfig(
               CANIVORE_NAME,
               20,
 
-              // KEEP
+              // UPDATED FOR OFFSEASON BOT
               99,
               148,
               1.75,
@@ -68,12 +84,23 @@ class CompConfig {
                       new CurrentLimitsConfigs()
                           .withStatorCurrentLimit(150)
                           .withSupplyCurrentLimit(150))
-                  .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(48))
+                  // UPDATED FOR OFFSEASON BOT
+                  .withFeedback(
+                      new FeedbackConfigs()
+                          .withSensorToMechanismRatio(
+                              (72.0 / 12.0) * (72.0 / 18.0) * (24.0 / 12.0)))
                   .withMotorOutput(
                       new MotorOutputConfigs()
                           .withInverted(InvertedValue.CounterClockwise_Positive)
                           .withNeutralMode(NeutralModeValue.Coast))
-                  .withSlot0(new Slot0Configs().withKP(200.0))),
+
+                  // TODO: BRINGUP: SET TO ZERO AND TUNE
+                  .withSlot0(
+                      new Slot0Configs()
+                          .withKP(200.0)
+                          .withKV(0.0)
+                          .withKG(0.0)
+                          .withGravityType(GravityTypeValue.Arm_Cosine))),
           new ClimberConfig(
               CANIVORE_NAME,
               21,
@@ -88,7 +115,8 @@ class CompConfig {
                           .withInverted(InvertedValue.CounterClockwise_Positive)
                           .withPeakReverseDutyCycle(0))
                   .withVoltage(new VoltageConfigs().withPeakReverseVoltage(0))
-                  .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(75.0))
+                  // TODO: WAITING FOR CORRECT RATIO
+                  .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(1.0))
                   .withCurrentLimits(
                       new CurrentLimitsConfigs()
                           .withStatorCurrentLimit(100)
@@ -97,6 +125,7 @@ class CompConfig {
               new CANcoderConfiguration()
                   .withMagnetSensor(
                       new MagnetSensorConfigs()
+                      // TODO: BRINGUP: RETUNE
                           .withMagnetOffset(-0.44921875)
                           .withSensorDirection(SensorDirectionValue.Clockwise_Positive)
                           .withAbsoluteSensorDiscontinuityPoint(0.5))),
@@ -115,6 +144,7 @@ class CompConfig {
                           .withSupplyCurrentLimit(150))
                   .withMotorOutput(
                       new MotorOutputConfigs()
+                      // TODO: BRINGUP: MAY NEED TO INVERT
                           .withInverted(InvertedValue.Clockwise_Positive)
                           .withNeutralMode(NeutralModeValue.Coast)),
               new TalonFXConfiguration()
@@ -124,9 +154,9 @@ class CompConfig {
                           .withSupplyCurrentLimit(150))
                   .withMotorOutput(
                       new MotorOutputConfigs()
+                      // TODO: BRINGUP: MAY NEED TO INVERT
                           .withInverted(InvertedValue.CounterClockwise_Positive)
                           .withNeutralMode(NeutralModeValue.Coast))),
-          // TODO: add radius and sensor-mechanism ratio
           new ElevatorConfig(
               CANIVORE_NAME,
               15,
@@ -142,17 +172,18 @@ class CompConfig {
                           .withStatorCurrentLimit(60))
                   .withMotorOutput(
                       new MotorOutputConfigs()
+                      // TODO: BRINGUP: MAY NEED TO INVERT
                           .withInverted(InvertedValue.Clockwise_Positive)
                           .withNeutralMode(NeutralModeValue.Brake))
                   .withSlot0(
                       new Slot0Configs()
-                          // kp 30.0
+                          // TODO: BRINGUP: REMOVE VALUES AND TUNE
                           .withKP(30.0)
                           .withKD(0.0)
                           .withKV(0)
-                          // kg 0.8
-                          .withKG(0.4)
+                          .withKG(0.0)
                           .withGravityType(GravityTypeValue.Elevator_Static))
+                          // TODO: BRINGUP: REMOVE VALUES AND TUNE
                   .withMotionMagic(
                       new MotionMagicConfigs()
                           .withMotionMagicAcceleration(300.0)
@@ -160,7 +191,8 @@ class CompConfig {
                   .withFeedback(
                       new FeedbackConfigs()
                           .withSensorToMechanismRatio(
-                              1.0 / ((8.0 / 40.0) * (2.0 / 1.0) * (Math.PI * 1.274))))),
+                            // UPDATED FOR OFFSEASON BOT
+                            (12.0/40.0)*(20.0/44.0) * (Math.PI * 1.88)))),
           new ArmConfig(
               RIO_CAN_NAME,
               18,
@@ -169,6 +201,7 @@ class CompConfig {
                       new MotorOutputConfigs()
                           .withNeutralMode(NeutralModeValue.Brake)
                           .withInverted(InvertedValue.CounterClockwise_Positive))
+                  // TODO: BRINGUP: REMOVE VALUES AND TUNE
                   .withSlot0(
                       new Slot0Configs()
                           .withKP(10.0)
@@ -176,19 +209,21 @@ class CompConfig {
                           .withKD(0.0)
                           .withKG(0.0)
                           .withGravityType(GravityTypeValue.Arm_Cosine))
+                  // TODO: BRINGUP: REMOVE VALUES
                   .withMotionMagic(
                       new MotionMagicConfigs()
                           .withMotionMagicAcceleration(100.0)
-                          .withMotionMagicCruiseVelocity(100.0)
-                          .withMotionMagicExpo_kA(0.0)
-                          .withMotionMagicExpo_kV(0.0))
+                          .withMotionMagicCruiseVelocity(100.0))
                   .withFeedback(
                       new FeedbackConfigs()
-                          .withSensorToMechanismRatio((64.0 / 8.0) * (90.0 / 10.0)))
+                      .withSensorToMechanismRatio(
+                            // UPDATED FOR OFFSEASON BOT
+                              (36.0 / 10.0) * (60.0 / 18.0) * (48.0 / 10.0)))
                   .withCurrentLimits(
                       new CurrentLimitsConfigs()
                           .withSupplyCurrentLimit(60.0)
                           .withStatorCurrentLimit(60.0)),
+                          // TODO: BRINGUP: RETUNE
               0.0),
           new VisionConfig(
               0.005,
