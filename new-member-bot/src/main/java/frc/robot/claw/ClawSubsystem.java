c6069278e9d8d426f4b9f70f0911ff3ebffe199b
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-10-25 15:26:41 -0700

    tunable claw

--- new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
+++ new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
@@ -11,7 +11,7 @@ import frc.robot.util.scheduling.SubsystemPriority;
 
 public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
   private final TalonFX motor;
-  private double minVelocity;
+  private double minVelocity = 5.0;
   private double minVelocityTimeout;
   private final Timer timeout = new Timer();
   private final Debouncer debouncer;
@@ -76,8 +76,12 @@ public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
 
   @Override
   public void whileInState(ClawState currentState) {
+    minVelocity = DogLog.tunable("Claw/MinVelocity", 5.0).get();
+    double tunableMaxVelocity = DogLog.tunable("Claw/MaxVeloctiy", RobotConfig.get().claw().gpMaxVelocity()).get();
     velocityDetectsGp =
-        getHasGp(motor.getVelocity().getValueAsDouble(), RobotConfig.get().claw().gpMaxVelocity());
+        getHasGp(motor.getVelocity().getValueAsDouble(), tunableMaxVelocity);
+    // velocityDetectsGp =
+    //     getHasGp(motor.getVelocity().getValueAsDouble(), RobotConfig.get().claw().gpMaxVelocity());
     DogLog.log("Claw/HasGP", getHasGP());
     DogLog.log("Claw/Motor/AppliedVoltage", motor.getMotorVoltage().getValueAsDouble());
     DogLog.log("Claw/Motor/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
