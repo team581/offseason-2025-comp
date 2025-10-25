9c3ed8edbddb2d796884ed502000144697fc5edd
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-10-25 13:14:55 -0700

    start autoblocks

---
+++ new-member-bot/src/main/java/frc/robot/autos/AutoBlocks.java
@@ -0,0 +1,17 @@
+package frc.robot.autos;
+
+import com.team581.trailblazer.Trailblazer;
+
+import frc.robot.robot_manager.RobotManager;
+
+public class AutoBlocks {
+  private final Trailblazer trailblazer;
+  private final RobotManager robotManager;
+  private final AutoCommands autoCommands;
+
+  public AutoBlocks(Trailblazer trailblazer, RobotManager robotManager, AutoCommands autoCommands) {
+    this.trailblazer = trailblazer;
+    this.robotManager = robotManager;
+    this.autoCommands = autoCommands;
+  }
+}
