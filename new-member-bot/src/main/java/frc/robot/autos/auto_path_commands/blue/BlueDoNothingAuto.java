6a1684b8f2dcce67029181f5bd2fd14de7715d9a
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-10-25 13:23:03 -0700

    blue do nothing auto

---
+++ new-member-bot/src/main/java/frc/robot/autos/auto_path_commands/blue/BlueDoNothingAuto.java
@@ -0,0 +1,37 @@
+
+package frc.robot.autos.auto_path_commands.blue;
+
+import com.team581.trailblazer.AutoPoint;
+import com.team581.trailblazer.AutoSegment;
+import com.team581.trailblazer.Trailblazer;
+import com.team581.trailblazer.constraints.AutoConstraintOptions;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.autos.BaseCommandAuto;
+import frc.robot.autos.Points;
+import frc.robot.robot_manager.RobotManager;
+
+public class BlueDoNothingAuto extends BaseCommandAuto {
+  private static final AutoConstraintOptions CONSTRAINTS = new AutoConstraintOptions(2, 57, 4, 30);
+
+  public BlueDoNothingAuto(RobotManager robotManager, Trailblazer trailblazer) {
+    super(robotManager, trailblazer);
+  }
+
+  @Override
+  public Pose2d getStartingPose() {
+    return Points.START_R1_AND_B1.bluePose;
+  }
+
+  @Override
+  protected Command createAutoCommand() {
+    return Commands.sequence(
+        trailblazer.followSegment(
+            new AutoSegment(
+                CONSTRAINTS,
+                new AutoPoint(Points.START_R1_AND_B1.bluePose),
+                new AutoPoint(new Pose2d(6.924, 7.292, Rotation2d.kZero)))));
+  }
+}
