59eb2089227a781656e34159cd5aaff9fbd42e9a
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-10-25 10:55:05 -0700

    remove candi for claw -> use motor velocity

--- new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
+++ new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
@@ -1,55 +1,51 @@
 package frc.robot.claw;
 
-import com.ctre.phoenix6.hardware.CANdi;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.S1StateValue;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.config.RobotConfig;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
   private final TalonFX motor;
-  private final CANdi candi;
-  private final Debouncer debouncer = RobotConfig.get().claw().debouncer();
+  private final double minVelocity;
+  private final double minVelocityTimeout;
+  private final Timer timeout = new Timer();
+  private final Debouncer debouncer;
+  private boolean hasSeenMinVelocity = false;
 
-  private boolean sensorRaw = false;
-  private boolean sensorDebounced = false;
 
-  public ClawSubsystem(TalonFX motor, CANdi candi) {
+  public ClawSubsystem(TalonFX motor, double minVelocity, double minVelocityTimeout, double debounceTime) {
     super(SubsystemPriority.CLAW, ClawState.IDLE_NO_GP);
 
     motor.getConfigurator().apply(RobotConfig.get().claw().motorConfig());
     this.motor = motor;
-    this.candi = candi;
+    this.minVelocity = minVelocity;
+    this.minVelocityTimeout = minVelocityTimeout;
+    this.debouncer = new Debouncer(debounceTime, DebounceType.kRising);
+    timeout.start();
   }
 
   @Override
   protected void collectInputs() {
-    var candiValue = candi.getS1State().getValue();
-    sensorRaw =
-        RobotConfig.get().claw().sensorFlipped()
-            ? candiValue == S1StateValue.Low
-            : candiValue != S1StateValue.Low;
+    hasSeenMinVelocity = false;
+    timeout.reset();
 
-    if (RobotBase.isSimulation()) {
-      sensorRaw =
-          switch (getState()) {
-            case IDLE_NO_GP -> false;
-            case IDLE_W_ALGAE, IDLE_W_CORAL -> true;
-            case INTAKING_ALGAE, INTAKING_CORAL -> timeout(1);
-            case SCORE_CORAL -> !timeout(0.5);
-            case SCORE_ALGAE_NET, SCORE_ALGAE_PROCESSOR, OUTTAKING -> !timeout(0.25);
-          };
-
-      sensorDebounced = debouncer.calculate(sensorRaw);
-    }
   }
 
-  public boolean getHasGP() {
-    return sensorDebounced;
+  public void reset() {
+    hasSeenMinVelocity = false;
+    timeout.reset();
+  }
+
+
+
+  public boolean getHasGP(double motorVelocity, double maxVelocity) {
+    hasSeenMinVelocity = hasSeenMinVelocity || timeout.hasElapsed(minVelocityTimeout) || motorVelocity >= minVelocity;
+    return hasSeenMinVelocity && debouncer.calculate(motorVelocity <= maxVelocity);
   }
 
   public void setState(ClawState newState) {
@@ -94,7 +90,5 @@ public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
     DogLog.log("Claw/Motor/AppliedVoltage", motor.getMotorVoltage().getValueAsDouble());
     DogLog.log("Claw/Motor/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
     DogLog.log("Claw/Motor/SupplyCurrent", motor.getSupplyCurrent().getValueAsDouble());
-    DogLog.log("Claw/Sensors/SensorRaw", sensorRaw);
-    DogLog.log("Claw/Sensors/SensorDebounced", sensorDebounced);
   }
 }
