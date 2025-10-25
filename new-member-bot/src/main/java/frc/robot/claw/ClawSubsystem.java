38c2f37d63f64f1e428dbb71c4f368e5315a47fb
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-10-25 16:03:01 -0700

    tunable claw

--- new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
+++ new-member-bot/src/main/java/frc/robot/claw/ClawSubsystem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Robot;
 import frc.robot.config.RobotConfig;
@@ -11,13 +12,15 @@ import frc.robot.util.scheduling.SubsystemPriority;
 
 public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
   private final TalonFX motor;
-  private double minVelocity = 5.0;
   private double minVelocityTimeout;
   private final Timer timeout = new Timer();
   private final Debouncer debouncer;
   private boolean hasSeenMinVelocity = false;
 
   private boolean velocityDetectsGp = false;
+
+  private DoubleSubscriber tunableMaxVelocity = DogLog.tunable("Claw/MaxVeloctiy", RobotConfig.get().claw().gpMaxVelocity());;
+  private DoubleSubscriber tunableMinVelocity = DogLog.tunable("Claw/MinVelocity", 5.0);
 
   public ClawSubsystem(TalonFX motor) {
     super(SubsystemPriority.CLAW, ClawState.IDLE_NO_GP);
@@ -52,7 +55,7 @@ public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
     hasSeenMinVelocity =
         hasSeenMinVelocity
             || timeout.hasElapsed(minVelocityTimeout)
-            || Math.abs(motorVelocity) >= minVelocity;
+            || Math.abs(motorVelocity) >= tunableMinVelocity.get();
 
     return hasSeenMinVelocity && debouncer.calculate(Math.abs(motorVelocity) <= maxVelocity);
   }
@@ -76,12 +79,7 @@ public class ClawSubsystem extends StateMachineSubsystem<ClawState> {
 
   @Override
   public void whileInState(ClawState currentState) {
-    minVelocity = DogLog.tunable("Claw/MinVelocity", 5.0).get();
-    double tunableMaxVelocity = DogLog.tunable("Claw/MaxVeloctiy", RobotConfig.get().claw().gpMaxVelocity()).get();
-    velocityDetectsGp =
-        getHasGp(motor.getVelocity().getValueAsDouble(), tunableMaxVelocity);
-    // velocityDetectsGp =
-    //     getHasGp(motor.getVelocity().getValueAsDouble(), RobotConfig.get().claw().gpMaxVelocity());
+    velocityDetectsGp = getHasGp(motor.getVelocity().getValueAsDouble(), tunableMaxVelocity.get());
     DogLog.log("Claw/HasGP", getHasGP());
     DogLog.log("Claw/Motor/AppliedVoltage", motor.getMotorVoltage().getValueAsDouble());
     DogLog.log("Claw/Motor/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
