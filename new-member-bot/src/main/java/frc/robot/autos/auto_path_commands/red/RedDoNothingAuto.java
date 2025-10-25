2492d126a0d42fa227bebc2d9c65d83b0438ae06
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-10-25 13:23:09 -0700

    red do nothing auto

---
+++ new-member-bot/src/main/java/frc/robot/autos/auto_path_commands/red/RedDoNothingAuto.java
@@ -0,0 +1,37 @@
+
+package frc.robot.autos.auto_path_commands.red;
+
+import com.team581.trailblazer.AutoPoint;
+import com.team581.trailblazer.AutoSegment;
+import com.team581.trailblazer.Trailblazer;
+import com.team581.trailblazer.constraints.AutoConstraintOptions;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.autos.BaseCommandAuto;
+import frc.robot.autos.Points;
+import frc.robot.robot_manager.RobotManager;
+
+public class RedDoNothingAuto extends BaseCommandAuto {
+  private static final AutoConstraintOptions CONSTRAINTS = new AutoConstraintOptions(2, 57, 4, 30);
+
+  public RedDoNothingAuto(RobotManager robotManager, Trailblazer trailblazer) {
+    super(robotManager, trailblazer);
+  }
+
+  @Override
+  public Pose2d getStartingPose() {
+    return Points.START_R1_AND_B1.redPose;
+  }
+
+  @Override
+  protected Command createAutoCommand() {
+    return Commands.sequence(
+        trailblazer.followSegment(
+            new AutoSegment(
+                CONSTRAINTS,
+                new AutoPoint(Points.START_R1_AND_B1.redPose),
+                new AutoPoint(new Pose2d(10.626, 0.758, Rotation2d.kZero)))));
+  }
+}
