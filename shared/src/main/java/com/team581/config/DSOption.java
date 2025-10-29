625b4137c8ff61de891bcd5677f151e194c26f05
Jonah Snider <jonah@jonahsnider.com>
2025-10-28 23:00:56 -0700

    Use entry instead of explicit publisher and subscriber in DSOption
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/config/DSOption.java
+++ shared/src/main/java/com/team581/config/DSOption.java
@@ -8,11 +8,11 @@ public class DSOption {
   private static final NetworkTable table = NetworkTableInstance.getDefault().getTable("DSOptions");
 
   public static BooleanSubscriber of(String key, boolean defaultValue) {
-    var topic = table.getBooleanTopic(key);
+    var entry = table.getBooleanTopic(key).getEntry(defaultValue);
 
-    topic.publish().setDefault(defaultValue);
+    entry.set(defaultValue);
 
-    return topic.subscribe(defaultValue);
+    return entry;
   }
 
   private DSOption() {}
