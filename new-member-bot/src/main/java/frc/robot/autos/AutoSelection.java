1025314d20e6dde8bb7a8227f3b2f8ef3d9d42de
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2025-11-04 21:49:22 -0800

    add auto to auto selection

--- new-member-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ new-member-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,11 +4,13 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_path_commands.blue.BlueDoNothingAuto;
 import frc.robot.autos.auto_path_commands.red.RedDoNothingAuto;
+import frc.robot.autos.auto_path_commands.red.RedMiddle2AlgaeAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
-  DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new);
+  DO_NOTHING(RedDoNothingAuto::new, BlueDoNothingAuto::new),
+  R3_ONE_CORAL_TWO_ALGAE(RedMiddle2AlgaeAuto::new, RedMiddle2AlgaeAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseCommandAuto> redAuto;
   public final BiFunction<RobotManager, Trailblazer, BaseCommandAuto> blueAuto;
