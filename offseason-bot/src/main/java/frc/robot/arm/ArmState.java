bc8034b0e270cf5bccefd81fecbc067888f1b77e
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-11-05 17:39:15 -0800

    Tune climber

--- offseason-bot/src/main/java/frc/robot/arm/ArmState.java
+++ offseason-bot/src/main/java/frc/robot/arm/ArmState.java
@@ -46,7 +46,7 @@ public enum ArmState {
 
   ALGAE_OUTTAKE(55),
 
-  CLIMBING(85);
+  CLIMBING(89);
 
   private final DoubleSubscriber tunableAngle;
 
--- offseason-bot/src/main/java/frc/robot/config/CompConfig.java
+++ offseason-bot/src/main/java/frc/robot/config/CompConfig.java
@@ -108,7 +108,7 @@ class CompConfig {
               21,
               22,
               -20.0,
-              170.0,
+              185.0,
               // Climb motor
               new TalonFXConfiguration()
                   .withMotorOutput(
@@ -129,7 +129,7 @@ class CompConfig {
                       new MagnetSensorConfigs()
                           .withMagnetOffset(-0.1064453125)
                           .withSensorDirection(SensorDirectionValue.Clockwise_Positive)
-                          .withAbsoluteSensorDiscontinuityPoint(0.5))),
+                          .withAbsoluteSensorDiscontinuityPoint(1.0))),
           new SingulatorConfig(
               CANIVORE_NAME,
               28,
