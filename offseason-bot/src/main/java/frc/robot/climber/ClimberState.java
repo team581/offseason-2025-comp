2200a2a8d073863eba30868afe0744b529fe8f42
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2025-11-07 21:54:26 -0800

    climber state changes

--- offseason-bot/src/main/java/frc/robot/climber/ClimberState.java
+++ offseason-bot/src/main/java/frc/robot/climber/ClimberState.java
@@ -3,9 +3,9 @@ package frc.robot.climber;
 public enum ClimberState {
   STOPPED(100.0, 0.0),
 
-  LINEUP_FORWARD(0, 1.5),
-  LINEUP_BACKWARD(30, 1.5),
-  HANGING(206.0, 12.0);
+  LINEUP_FORWARD(0, 2.5),
+  LINEUP_BACKWARD(30, 2.5),
+  HANGING(185.0, 12.0);
 
   public final double angle;
   public final double forwardsVoltage;
